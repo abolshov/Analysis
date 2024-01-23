5c78e6a212920d33293cf1d247a9e63a01d4807e
abolshov <artem.e.bolshov@gmail.com>
2024-01-23 15:40:55 -0600

    add tools for searching descendants

---
+++ matching/MatchingTools.cpp
@@ -0,0 +1,32 @@
+#include "MatchingTools.hpp"
+
+std::vector<int> GetNextGeneration(int part_idx, int const* mothers, int n_gen_part)
+{
+    std::vector<int> gen;
+    for (int i = 0; i < n_gen_part; ++i)
+    {
+        if (mothers[i] == part_idx) gen.push_back(i);
+    }
+    return gen;
+}
+
+std::vector<std::vector<int>> GetDescendants(int part_idx, int const* mothers, int n_gen_part)
+{
+    std::vector<std::vector<int>> descendants;
+    descendants.push_back({part_idx});
+    int gener_counter = 1;
+    auto cur_gen = GetNextGeneration(part_idx, mothers, n_gen_part);
+    while (!cur_gen.empty())
+    {
+        descendants.push_back(cur_gen);
+        std::vector<int> next_gen;
+        for (auto const& part: cur_gen)
+        {
+            auto part_daughters = GetNextGeneration(part, mothers, n_gen_part);
+            next_gen.insert(next_gen.end(), part_daughters.begin(), part_daughters.end());
+        }
+        ++gener_counter;
+        cur_gen = next_gen;
+    }
+    return descendants;
+}
---
+++ matching/MatchingTools.hpp
@@ -0,0 +1,13 @@
+#ifndef M_TOOLS_HPP
+#define M_TOOLS_HPP
+
+#include <vector>
+
+// finds closest daughters of particle at location part_idx in the event;
+std::vector<int> GetNextGeneration(int part_idx, int const* mothers, int n_gen_part);
+
+// finds all descendants of particle at location part_idx in the event;
+// descendants are grouped into generations (inner vectors): generation 0 is particle itself, generation 1 are its immediate daughters, etc ...
+std::vector<std::vector<int>> GetDescendants(int part_idx, int const* mothers, int n_gen_part);
+
+#endif
