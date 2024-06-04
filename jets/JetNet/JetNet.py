eeb8d35732cd63694528ef37e93290801e91cb03
abolshov <artem.e.bolshov@gmail.com>
2024-06-04 11:54:16 -0500

    reorganize JetNet code to make it work with JetNetData

--- jets/JetNet/JetNet.py
+++ jets/JetNet/JetNet.py
@@ -6,17 +6,9 @@ import uproot
 from JetNet_utils import MXLossFunc
 
 class JetNet():
-    def __init__(self):
-        self.features = ['genbjet1_px', 'genbjet1_py', 'genbjet1_pz', 'genbjet1_E', 'genbjet2_px', 'genbjet2_py', 'genbjet2_pz', 'genbjet2_E']
-        # self.labels = ['H_WW_px', 'H_WW_py', 'H_WW_pz', 'H_WW_E', 'X_px', 'X_py', 'X_pz', 'X_E']
-        self.labels = ['H_WW_px', 'H_WW_py', 'H_WW_pz', 'H_WW_E', 'X_mass']
-
-        # dataset for training
-        self.train_features = pd.DataFrame()
-        self.test_features = pd.DataFrame()
-
-        self.train_labels = pd.DataFrame()
-        self.test_labels = pd.DataFrame()
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
 
         # training parameters
         self.lr = 0.001
@@ -24,55 +16,36 @@ class JetNet():
         self.batch_size = 64
         self.verbosity = 0
         self.valid_split = 0.33
-        self.train_frac = 0.8
 
         self.model = None
-    
-
-    def ReadFile(self, file_name):
-        file = uproot.open(file_name)
-        tree = file['JetNetTree']
-        branches = tree.arrays()
-
-        data_dict = {}
-        for name in tree.keys():
-            data_dict[name] = np.array(branches[name])
-            
-        df = pd.DataFrame.from_dict(data_dict)
-
-        train_df = df.sample(frac=self.train_frac, random_state=0)
-        test_df = df.drop(train_df.index)
-
-        self.train_features = train_df[self.features].copy()
-        self.test_features = test_df[self.features].copy()
-        self.train_labels = train_df[self.labels]
-        self.test_labels = test_df[self.labels]
 
 
-    def ConfigureModel(self):
-        self.model = tf.keras.Sequential([tf.keras.layers.Dense(32, activation='relu'),
+    def ConfigureModel(self, dataset_shape):
+        self.model = tf.keras.Sequential([tf.keras.layers.Dense(64, activation='relu'),
                                           tf.keras.layers.Dense(32, activation='relu'),
-                                          tf.keras.layers.Dense(32, activation='relu'),
+                                          tf.keras.layers.Dense(16, activation='relu'),
                                           tf.keras.layers.Dense(3)])
 
         self.model.compile(loss=MXLossFunc, optimizer=tf.keras.optimizers.Adam(self.lr))
-        self.model.build(self.train_features.shape)
+        self.model.build(dataset_shape)
 
 
-    def Fit(self):
-        self.ConfigureModel()
-        history = self.model.fit(self.train_features,
-                                 self.train_labels,
+    def Fit(self, train_features, train_labels):
+        if not self.model:
+            raise RuntimeError("Model has not been configured before fitting")
+        history = self.model.fit(train_features,
+                                 train_labels,
                                  validation_split=self.valid_split,
                                  verbose=self.verbosity,
                                  batch_size=self.batch_size,
                                  epochs=self.n_epochs)
-
         return history
 
 
-    def Predict(self):
-        return self.model.predict(self.test_features)
+    def Predict(self, test_features):
+        if not np.all(test_features.columns == self.features):
+            raise RuntimeError(f"Features pased for prediction do not match expected features: passed {test_features.columns}, while expected {self.features}")
+        return self.model.predict(test_features)
 
 
     def SaveModel(self, path):
--- jets/JetNet/train_JetNet.py
+++ jets/JetNet/train_JetNet.py
@@ -1,16 +1,27 @@
 from JetNet import JetNet
+from JetNetData import JetNetData
 from JetNet_utils import PlotLoss, PlotPrediction
 
 
 def main():
-    net = JetNet()
-    input_file = "../JetNetTrain_M-500.root"
-    net.ReadFile(input_file)
-    history = net.Fit()
+    features = ['genbjet1_px', 'genbjet1_py', 'genbjet1_pz', 'genbjet1_E', 'genbjet2_px', 'genbjet2_py', 'genbjet2_pz', 'genbjet2_E']
+    labels = ['H_WW_px', 'H_WW_py', 'H_WW_pz', 'H_WW_E', 'X_mass']
+    input_files = ["../JetNetTrain_M-500.root"]
+
+    data = JetNetData(features, labels)
+    data.ReadFiles(input_files)
+    data.Shuffle()
+    if len(input_files) == 1:
+        data.TrainTestSplit()
+
+    net = JetNet(features, labels)
+    net.ConfigureModel(data.train_features.shape)
+    history = net.Fit(data.train_features, data.train_labels)
     net.SaveModel("./models/")
     PlotLoss(history)
-    pred = net.Predict()
-    PlotPrediction(pred, net.test_labels)
+    if len(input_files) == 1:
+        pred = net.Predict(data.test_features)
+        PlotPrediction(pred, data.test_labels)
 
     
 if __name__ == '__main__':
