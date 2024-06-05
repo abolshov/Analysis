368439fd1bb933bfb801c9423f1bdf87d1c1fd52
abolshov <artem.e.bolshov@gmail.com>
2024-06-05 13:50:48 -0500

    update testing script

--- jets/JetNet/test_JetNet.py
+++ jets/JetNet/test_JetNet.py
@@ -1,14 +1,22 @@
 from JetNet import JetNet
+from JetNetData import JetNetData
 from JetNet_utils import PlotPrediction
 
+import numpy as np
 
 def main():
-    net = JetNet()
-    input_file = "../JetNetTrain_M-700.root"
+    features = ['genbjet1_px', 'genbjet1_py', 'genbjet1_pz', 'genbjet1_E', 'genbjet2_px', 'genbjet2_py', 'genbjet2_pz', 'genbjet2_E']
+    labels = ['H_WW_px', 'H_WW_py', 'H_WW_pz', 'H_WW_E', 'X_mass']
+    input_files = ["../JetNetTrain_M-1000.root"]
+
+    data = JetNetData(features, labels)
+    data.ReadFiles(input_files)
+
+    net = JetNet(features, labels)
     net.LoadModel("./models/JetNet_v1.keras")
-    net.ReadFile(input_file)
-    pred = net.Predict()
-    PlotPrediction(pred, net.test_labels)
+
+    pred = net.Predict(data.data[features])
+    PlotPrediction(pred, data.data)
 
     
 if __name__ == '__main__':
