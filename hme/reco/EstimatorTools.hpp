f2d814103d467f3d5e89b62d960ce44c89fc1fd9
abolshov <artem.e.bolshov@gmail.com>
2024-09-13 17:46:22 -0500

    update reco hme code

---
+++ hme/reco/EstimatorTools.hpp
@@ -0,0 +1,57 @@
+#ifndef ESTIMATOR_TOOLS_HPP
+#define ESTIMATOR_TOOLS_HPP
+
+#include <optional>
+#include <vector>
+#include <memory>
+
+#include "TLorentzVector.h"
+#include "TRandom3.h"
+#include "TH1.h"
+#include "TH2.h"
+
+// general parameters
+inline constexpr double HIGGS_MASS = 125.03;
+inline constexpr double HIGGS_WIDTH = 0.004;
+inline constexpr double TOL = 10e-7;
+inline constexpr int N_ATTEMPTS = 1;
+inline constexpr int N_ITER = 1000;
+
+// event mass distribution parameters
+inline constexpr double MAX_MASS = 1500;
+inline constexpr int N_BINS = 1500;
+
+using OptionalPair = std::optional<std::pair<double, double>>;
+
+enum PhysObj { bj1, bj2, wj1, wj2, lep, met };
+enum PDF { lead_bjet_pt_pdf, subl_bjet_pt_pdf, lead_bjet_E_pdf, subl_bjet_E_pdf, lead_bjet_pz_pdf, subl_bjet_pz_pdf };
+
+// computes rescaling factors for jets using rescale pdf and mass of particle->jj
+// has side effect: may swap j1 and j2
+OptionalPair JetRescFact(TLorentzVector& j1, TLorentzVector& j2, std::unique_ptr<TH1F>& pdf, double mass);
+
+// compute 4-momentum of neutrino from W->lv using Higgs H->WW mass constraint
+std::optional<TLorentzVector> ComputeNu(TLorentzVector const& l, TLorentzVector const& j1, TLorentzVector const& j2, TLorentzVector const& met, double mh, double eta);
+std::optional<TLorentzVector> ComputeNu(TLorentzVector const& l, TLorentzVector const& j1, TLorentzVector const& j2, double mh, double eta, double phi);
+std::optional<TLorentzVector> ComputeNu(TLorentzVector const& l, TLorentzVector const& met, double mw, double eta);
+
+// this is HME: samples pdfs, computes corrections and calculates mass distribution for an event
+// in case of success returns mass and fraction of iterations which computed mass
+OptionalPair EstimateMass(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& b_resc_pdf, TRandom3& rg, int evt);
+
+namespace Experimental
+{
+    OptionalPair EstimateMassIdealEtaPhi(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& b_resc_pdf, TRandom3& rg, int evt, std::pair<double, double> const& dir);
+    OptionalPair EstimateMassIdealNu(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& lead_b_pdf, TRandom3& rg, int evt, TLorentzVector const& nu);
+    OptionalPair EstimateMassIdealNu2dPDF(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH2F>& bjet_2d_pdf, TRandom3& rg, int evt, TLorentzVector const& nu);
+    // OptionalPair EstimateMassNew(std::vector<TLorentzVector> const& particles, std::vector<std::unique_ptr<TH1F>> const& pdfs, TRandom3& rg, int evt, TLorentzVector const& nu);
+    OptionalPair EstimateMassIdealHbb(std::vector<TLorentzVector> const& particles, TRandom3& rg, int evt); 
+    OptionalPair EstimateMassIdealHWW(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& pdf, TRandom3& rg, int evt, TLorentzVector const& HtoWW);
+    OptionalPair EstimateMassNoMETCorr(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& pdf, TRandom3& rg, int evt); 
+    OptionalPair EstimateMassRandMETFrac(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& pdf, std::unique_ptr<TH1F>& met_pdf, TRandom3& rg, int evt); 
+    OptionalPair EstimateMassRandWmass(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& pdf, TRandom3& rg, int evt); 
+    OptionalPair EstimateMassWeightRescIdealNu(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH1F>& pdf, std::unique_ptr<TH1F>& pt_corr_pdf, std::unique_ptr<TH1F>& mass_pdf, TRandom3& rg, int evt, TLorentzVector const& nu, double& mass, double& integral);
+    OptionalPair EstimateMassWeightResc2dPDFIdealNu(std::vector<TLorentzVector> const& particles, std::unique_ptr<TH2F>& pdf, std::unique_ptr<TH1F>& pt_corr_pdf, std::unique_ptr<TH1F>& mass_pdf, TRandom3& rg, int evt, TLorentzVector const& nu);    
+}
+
+#endif
--- hme/reco/reco_hme.cpp
+++ hme/reco/reco_hme.cpp
@@ -2,293 +2,207 @@
 #include <algorithm>
 #include <numeric>
 #include <memory>
-#include <fstream>
-#include <optional>
 #include <chrono>
-#include <cassert>
 
 #include "TTree.h"
 #include "TFile.h"
 #include "TLorentzVector.h"
 #include "TH1.h"
-#include "TString.h"
 #include "TRandom3.h"
 
 #include "HistManager.hpp"
+#include "EstimatorTools.hpp"
+#include "CombTools.hpp"
 
-using OptPair = std::optional<std::pair<double, double>>;
-
-inline constexpr int N_ATTEMPTS = 10;
-inline constexpr int N_ITER = 10000;
-
-inline constexpr double HIGGS_MASS = 125.03;
-inline constexpr double HIGGS_WIDTH = 0.004;
-
-inline constexpr double MAX_MASS = 1500;
-inline constexpr int N_BINS = 1500;
-
-inline constexpr double TOL = 10e-7;
-
-inline constexpr bool USE_RECO = true;
-inline constexpr bool CHEAT = false;
-
-template <typename T>
-std::vector<int> sort_indices(T* v, int n) 
-{
-  std::vector<int> idx(n);
-  std::iota(idx.begin(), idx.end(), 0);
-  std::stable_sort(idx.begin(), idx.end(), [&v](int i1, int i2) {return v[i1] > v[i2];});
-  return idx;
-}
-
-OptPair JetRescFact(TLorentzVector& j1, TLorentzVector& j2, std::unique_ptr<TH1F>& pdf, double mass)
-{
-    if (j1.Pt() < j2.Pt()) 
-    {
-        std::swap(j1, j2);
-    }
-
-    for (int i = 0; i < N_ATTEMPTS; ++i)
-    {
-        double c1 = pdf->GetRandom();
-    
-        double x1 = j2.M2();
-        double x2 = 2*c1*(j1*j2);
-        double x3 = c1*c1*j1.M2() - mass*mass;
-
-        double discriminant = (x2*x2 - 4.0*x1*x3);
-
-        if (x2 < 0.0)
-        {
-            continue;
-        }
-        if (discriminant < 0.0 || std::abs(x1) < TOL)
-        {
-            continue;
-        }
-
-        double c2 = (-1.0*x2 + sqrt(discriminant))/(2*x1);
-
-        if (c2 < 0.0)
-        {
-            continue;
-        }
-
-        return std::make_optional<std::pair<double, double>>(c1, c2);
-    }
-
-    return std::nullopt;
-}
+static constexpr int N_RECO_JETS = 12;
 
 int main()
 {
     auto start = std::chrono::system_clock::now();
-    std::cout << std::boolalpha;
-    std::cout << "Regime:\n";
-    std::cout << "  CHEAT: " << CHEAT << "\n";
-    std::cout << "  USE_RECO: " << USE_RECO << "\n";
 
-    std::unique_ptr<TFile> file_pdf;
-    std::unique_ptr<TH1F> pdf;
+    auto file_pdf = std::make_unique<TFile>("pdf.root", "READ");
+    auto pdf = std::unique_ptr<TH1F>(static_cast<TH1F*>(file_pdf->Get("pdf")));
 
-    if constexpr (USE_RECO)
-    {
-        file_pdf = std::make_unique<TFile>("pdf.root", "READ");
-        pdf = std::unique_ptr<TH1F>(static_cast<TH1F*>(file_pdf->Get("pdf")));
-    }
-
-    if constexpr (!USE_RECO)
-    {
-        file_pdf = std::make_unique<TFile>("pdfs.root", "READ");
-        pdf = std::unique_ptr<TH1F>(static_cast<TH1F*>(file_pdf->Get("lead_bjet_pdf")));    
-    }
-
-    TFile *myFile = TFile::Open("GluGlutoRadiontoHHto2B2Vto2B2JLNu_M-450/nano_0.root");
+    TFile *myFile = TFile::Open("nano_0.root");
     TTree *myTree = static_cast<TTree*>(myFile->Get("Events"));
 
-    Double_t        genHVV_pt;
-    Double_t        genHVV_eta;
-    Double_t        genHVV_phi;
-    Double_t        genHVV_mass;
+    TRandom3 rg;
+    rg.SetSeed(42);
+
+    // reco variables
+    Float_t         lep1_pt;
+    Float_t         lep1_eta;
+    Float_t         lep1_phi;
+    Float_t         lep1_mass;
+
+    Float_t         PuppiMET_pt;
+    Float_t         PuppiMET_phi;
 
     Int_t           ncentralJet;
-    Float_t         centralJet_pt[20];   //[ncentralJet]
-    Float_t         centralJet_eta[20];   //[ncentralJet]
-    Float_t         centralJet_phi[20];   //[ncentralJet]
-    Float_t         centralJet_mass[20];   //[ncentralJet]
+    Float_t         centralJet_pt[N_RECO_JETS];   
+    Float_t         centralJet_eta[N_RECO_JETS];   
+    Float_t         centralJet_phi[N_RECO_JETS];   
+    Float_t         centralJet_mass[N_RECO_JETS]; 
+    Float_t         centralJet_btagPNetB[N_RECO_JETS]; 
 
-    Float_t         centralJet_btagPNetB[20];   //[ncentralJet]
-    Float_t         centralJet_btagDeepFlavB[20];   //[ncentralJet]
+    myTree->SetBranchAddress("lep1_pt", &lep1_pt);
+    myTree->SetBranchAddress("lep1_eta", &lep1_eta);
+    myTree->SetBranchAddress("lep1_phi", &lep1_phi);
+    myTree->SetBranchAddress("lep1_mass", &lep1_mass);
 
-    Double_t        genb1_pt;
-    Double_t        genb1_eta;
-    Double_t        genb1_phi;
-    Double_t        genb1_mass;
-
-    Double_t        genb2_pt;
-    Double_t        genb2_eta;
-    Double_t        genb2_phi;
-    Double_t        genb2_mass;
-
-    Double_t        genb1_vis_pt;
-    Double_t        genb1_vis_eta;
-    Double_t        genb1_vis_phi;
-    Double_t        genb1_vis_mass;
-
-    Double_t        genb2_vis_pt;
-    Double_t        genb2_vis_eta;
-    Double_t        genb2_vis_phi;
-    Double_t        genb2_vis_mass;
-
-    myTree->SetBranchAddress("genHVV_pt", &genHVV_pt);
-    myTree->SetBranchAddress("genHVV_eta", &genHVV_eta);
-    myTree->SetBranchAddress("genHVV_phi", &genHVV_phi);
-    myTree->SetBranchAddress("genHVV_mass", &genHVV_mass);
+    myTree->SetBranchAddress("PuppiMET_pt", &PuppiMET_pt);
+    myTree->SetBranchAddress("PuppiMET_phi", &PuppiMET_phi);
 
     myTree->SetBranchAddress("ncentralJet", &ncentralJet);
-    myTree->SetBranchAddress("centralJet_pt", &centralJet_pt);
-    myTree->SetBranchAddress("centralJet_eta", &centralJet_eta);
-    myTree->SetBranchAddress("centralJet_phi", &centralJet_phi);
-    myTree->SetBranchAddress("centralJet_mass", &centralJet_mass);
+    myTree->SetBranchAddress("centralJet_pt", centralJet_pt);
+    myTree->SetBranchAddress("centralJet_eta", centralJet_eta);
+    myTree->SetBranchAddress("centralJet_phi", centralJet_phi);
+    myTree->SetBranchAddress("centralJet_mass", centralJet_mass);
+    myTree->SetBranchAddress("centralJet_btagPNetB", centralJet_btagPNetB);
 
-    myTree->SetBranchAddress("centralJet_btagPNetB", &centralJet_btagPNetB);
-    myTree->SetBranchAddress("centralJet_btagDeepFlavB", &centralJet_btagDeepFlavB);
+    // gen variables
+    // V1: leptonic 
+    // prod1: lep
+    // prod2: neutrino
+    Double_t        genV1prod1_pt;
+    Double_t        genV1prod1_vis_pt;
+    Double_t        genV1prod1_eta;
+    Double_t        genV1prod1_vis_eta;
+    Double_t        genV1prod1_phi;
+    Double_t        genV1prod1_vis_phi;
+    Double_t        genV1prod1_mass;
+    Double_t        genV1prod1_vis_mass;
 
-    myTree->SetBranchAddress("genb1_pt", &genb1_pt);
-    myTree->SetBranchAddress("genb1_eta", &genb1_eta);
-    myTree->SetBranchAddress("genb1_phi", &genb1_phi);
-    myTree->SetBranchAddress("genb1_mass", &genb1_mass);
+    Double_t        genV1prod2_pt;
+    Double_t        genV1prod2_vis_pt;
+    Double_t        genV1prod2_eta;
+    Double_t        genV1prod2_vis_eta;
+    Double_t        genV1prod2_phi;
+    Double_t        genV1prod2_vis_phi;
+    Double_t        genV1prod2_mass;
+    Double_t        genV1prod2_vis_mass;
 
-    myTree->SetBranchAddress("genb2_pt", &genb2_pt);
-    myTree->SetBranchAddress("genb2_eta", &genb2_eta);
-    myTree->SetBranchAddress("genb2_phi", &genb2_phi);
-    myTree->SetBranchAddress("genb2_mass", &genb2_mass);
+    // V2: hadronic
+    Double_t        genV2prod1_pt;
+    Double_t        genV2prod1_vis_pt;
+    Double_t        genV2prod1_eta;
+    Double_t        genV2prod1_vis_eta;
+    Double_t        genV2prod1_phi;
+    Double_t        genV2prod1_vis_phi;
+    Double_t        genV2prod1_mass;
+    Double_t        genV2prod1_vis_mass;
 
-    myTree->SetBranchAddress("genb1_vis_pt", &genb1_vis_pt);
-    myTree->SetBranchAddress("genb1_vis_eta", &genb1_vis_eta);
-    myTree->SetBranchAddress("genb1_vis_phi", &genb1_vis_phi);
-    myTree->SetBranchAddress("genb1_vis_mass", &genb1_vis_mass);
+    Double_t        genV2prod2_pt;
+    Double_t        genV2prod2_vis_pt;
+    Double_t        genV2prod2_eta;
+    Double_t        genV2prod2_vis_eta;
+    Double_t        genV2prod2_phi;
+    Double_t        genV2prod2_vis_phi;
+    Double_t        genV2prod2_mass;
+    Double_t        genV2prod2_vis_mass;
 
-    myTree->SetBranchAddress("genb2_vis_pt", &genb2_vis_pt);
-    myTree->SetBranchAddress("genb2_vis_eta", &genb2_vis_eta);
-    myTree->SetBranchAddress("genb2_vis_phi", &genb2_vis_phi);
-    myTree->SetBranchAddress("genb2_vis_mass", &genb2_vis_mass);
+    myTree->SetBranchAddress("genV1prod1_pt", &genV1prod1_pt);
+    myTree->SetBranchAddress("genV1prod1_vis_pt", &genV1prod1_vis_pt);
+    myTree->SetBranchAddress("genV1prod1_eta", &genV1prod1_eta);
+    myTree->SetBranchAddress("genV1prod1_vis_eta", &genV1prod1_vis_eta);
+    myTree->SetBranchAddress("genV1prod1_phi", &genV1prod1_phi);
+    myTree->SetBranchAddress("genV1prod1_vis_phi", &genV1prod1_vis_phi);
+    myTree->SetBranchAddress("genV1prod1_mass", &genV1prod1_mass);
+    myTree->SetBranchAddress("genV1prod1_vis_mass", &genV1prod1_vis_mass);
+
+    myTree->SetBranchAddress("genV1prod2_pt", &genV1prod2_pt);
+    myTree->SetBranchAddress("genV1prod2_vis_pt", &genV1prod2_vis_pt);
+    myTree->SetBranchAddress("genV1prod2_eta", &genV1prod2_eta);
+    myTree->SetBranchAddress("genV1prod2_vis_eta", &genV1prod2_vis_eta);
+    myTree->SetBranchAddress("genV1prod2_phi", &genV1prod2_phi);
+    myTree->SetBranchAddress("genV1prod2_vis_phi", &genV1prod2_vis_phi);
+    myTree->SetBranchAddress("genV1prod2_mass", &genV1prod2_mass);
+    myTree->SetBranchAddress("genV1prod2_vis_mass", &genV1prod2_vis_mass);
+
+    myTree->SetBranchAddress("genV2prod1_pt", &genV2prod1_pt);
+    myTree->SetBranchAddress("genV2prod1_vis_pt", &genV2prod1_vis_pt);
+    myTree->SetBranchAddress("genV2prod1_eta", &genV2prod1_eta);
+    myTree->SetBranchAddress("genV2prod1_vis_eta", &genV2prod1_vis_eta);
+    myTree->SetBranchAddress("genV2prod1_phi", &genV2prod1_phi);
+    myTree->SetBranchAddress("genV2prod1_vis_phi", &genV2prod1_vis_phi);
+    myTree->SetBranchAddress("genV2prod1_mass", &genV2prod1_mass);
+    myTree->SetBranchAddress("genV2prod1_vis_mass", &genV2prod1_vis_mass);
+
+    myTree->SetBranchAddress("genV2prod2_pt", &genV2prod2_pt);
+    myTree->SetBranchAddress("genV2prod2_vis_pt", &genV2prod2_vis_pt);
+    myTree->SetBranchAddress("genV2prod2_eta", &genV2prod2_eta);
+    myTree->SetBranchAddress("genV2prod2_vis_eta", &genV2prod2_vis_eta);
+    myTree->SetBranchAddress("genV2prod2_phi", &genV2prod2_phi);
+    myTree->SetBranchAddress("genV2prod2_vis_phi", &genV2prod2_vis_phi);
+    myTree->SetBranchAddress("genV2prod2_mass", &genV2prod2_mass);
+    myTree->SetBranchAddress("genV2prod2_vis_mass", &genV2prod2_vis_mass);
 
     HistManager hm;
 
-    std::string res_mass_default("res_mass_default");
-    hm.Add(res_mass_default, "X->HH mass", {"X->HH mass, [GeV]", "Count"}, {0, 1000}, 250);
-
     std::string hme_mass("hme_mass");
     hm.Add(hme_mass, "HME X->HH mass", {"X->HH mass, [GeV]", "Count"}, {0, 1000}, 250);
-
-    std::string succ_rate("succ_rate");
-    hm.Add(succ_rate, "HME Success rate", {"Success rate", "Count"}, {0, 1}, 10);
-
-    TRandom3 rg;
-    rg.SetSeed(0);
 
     int nEvents = myTree->GetEntries();
     for (int i = 0; i < nEvents; ++i)
     {
         myTree->GetEntry(i);
-
-        TLorentzVector bj1, bj2;
-        int j1, j2;
-        if constexpr (USE_RECO)
+        if (ncentralJet < 4)
         {
-            std::vector<int> sorted_by_PNet = sort_indices<Float_t>(centralJet_btagPNetB, ncentralJet);
-            // std::vector<int> sorted_by_DeepFlav = sort_indices<Float_t>(centralJet_btagDeepFlavB, ncentralJet);
-
-            j1 = sorted_by_PNet[0];
-            j2 = sorted_by_PNet[1];
-
-            bj1.SetPtEtaPhiM(centralJet_pt[j1], centralJet_eta[j1], centralJet_phi[j1], centralJet_mass[j1]);
-            bj2.SetPtEtaPhiM(centralJet_pt[j2], centralJet_eta[j2], centralJet_phi[j2], centralJet_mass[j2]);
+            continue;
         }
 
-        if constexpr (!USE_RECO)
+        TLorentzVector reco_lep_p4, reco_bj1_p4, reco_bj2_p4, reco_met_p4;
+        reco_lep_p4.SetPtEtaPhiM(lep1_pt, lep1_eta, lep1_phi, lep1_mass);
+        reco_met_p4.SetPtEtaPhiM(PuppiMET_pt, 0.0, PuppiMET_phi, 0.0);
+
+        // establish jet with greater pt for computing corrections
+        // jets are sorted by btag so first two are b jets
+        if (centralJet_pt[0] > centralJet_pt[1])
         {
-            bj1.SetPtEtaPhiM(genb1_vis_pt, genb1_vis_eta, genb2_vis_phi, genb2_vis_mass);
-            bj2.SetPtEtaPhiM(genb2_vis_pt, genb2_vis_eta, genb2_vis_phi, genb2_vis_mass);
+            reco_bj1_p4.SetPtEtaPhiM(centralJet_pt[0], centralJet_eta[0], centralJet_phi[0], centralJet_mass[0]);
+            reco_bj2_p4.SetPtEtaPhiM(centralJet_pt[1], centralJet_eta[1], centralJet_phi[1], centralJet_mass[1]);
+        }
+        else 
+        {
+            reco_bj2_p4.SetPtEtaPhiM(centralJet_pt[0], centralJet_eta[0], centralJet_phi[0], centralJet_mass[0]);
+            reco_bj1_p4.SetPtEtaPhiM(centralJet_pt[1], centralJet_eta[1], centralJet_phi[1], centralJet_mass[1]);
         }
 
-        bool first_match = true;
-        bool second_match = true;
-
-        if constexpr (CHEAT)
+        std::vector<TLorentzVector> light_jets;
+        for (int j = 2; j < ncentralJet; ++j)
         {
-            if constexpr (USE_RECO)
-            {
-                TLorentzVector bq1, bq2;
-                bq1.SetPtEtaPhiM(genb1_pt, genb1_eta, genb1_phi, genb1_mass);
-                bq2.SetPtEtaPhiM(genb2_pt, genb2_eta, genb2_phi, genb2_mass);
-
-                first_match = bq1.DeltaR(bj1) < 0.4 || bq1.DeltaR(bj2) < 0.4;
-                second_match = bq2.DeltaR(bj1) < 0.4 || bq2.DeltaR(bj2) < 0.4;
-            }
-            else 
-            {
-                first_match = genb1_vis_pt > 20 && std::abs(genb1_vis_eta) < 2.5;
-                second_match = genb2_vis_pt > 20 && std::abs(genb2_vis_eta) < 2.5;
-            }
+            TLorentzVector jet;
+            jet.SetPtEtaPhiM(centralJet_pt[i], centralJet_eta[i], centralJet_phi[i], centralJet_mass[i]);
+            light_jets.push_back(jet);
         }
 
-        if (first_match && second_match)
+        auto best_onshell_pair = ChooseBestPair(light_jets, [](TLorentzVector const& p1, TLorentzVector const& p2){ return std::abs(80.0 - (p1 + p2).M()); });
+        auto best_offshell_pair = ChooseBestPair(light_jets, [](TLorentzVector const& p1, TLorentzVector const& p2){ return std::abs(40.0 - (p1 + p2).M()); });
+
+        auto [idx1, idx2] = best_onshell_pair;
+        std::vector<TLorentzVector> input_onshell = {reco_bj1_p4, reco_bj2_p4, light_jets[idx1], light_jets[idx2], reco_lep_p4, reco_met_p4};
+
+        std::tie(idx1, idx2) = best_offshell_pair;
+        std::vector<TLorentzVector> input_offshell = {reco_bj1_p4, reco_bj2_p4, light_jets[idx1], light_jets[idx2], reco_lep_p4, reco_met_p4};
+
+        auto hme = EstimateMass(input_onshell, pdf, rg, i);
+        if (hme)
         {
-            TLorentzVector HtoVV;
-            HtoVV.SetPtEtaPhiM(genHVV_pt, genHVV_eta, genHVV_phi, genHVV_mass);
+            auto [mass, eff] = hme.value();
+            hm.FillWeighted(hme_mass, mass, 0.5);
+        }
 
-            auto evt_mass = std::make_unique<TH1F>("X_mass", Form("X->HH mass: event %d", i), N_BINS, 0.0, MAX_MASS);
-            int failed_iter = 0;
-            for (int iter = 0; iter < N_ITER; ++iter)
-            {
-                double mh = rg.Gaus(HIGGS_MASS, HIGGS_WIDTH);
-                OptPair b_jet_resc = JetRescFact(bj1, bj2, pdf, mh);
-                if (b_jet_resc)
-                {
-                    auto [c1, c2] = b_jet_resc.value();
-                    TLorentzVector bb1 = c1*bj1;
-                    TLorentzVector bb2 = c2*bj2;
-
-                    TLorentzVector tmp(HtoVV);
-                    HtoVV.SetPtEtaPhiM(genHVV_pt, genHVV_eta, genHVV_phi, genHVV_mass);
-                    tmp += bb1;
-                    tmp += bb2;
-
-                    double X_mass = tmp.M();
-                    evt_mass->Fill(X_mass);
-                }
-                else 
-                {
-                    ++failed_iter;
-                    continue;
-                }
-            }
-
-            if (evt_mass->GetEntries())
-            {
-                int binmax = evt_mass->GetMaximumBin(); 
-                double mass = evt_mass->GetXaxis()->GetBinCenter(binmax);
-                double success_rate = 1.0 - static_cast<double>(failed_iter)/N_ITER;
-
-                hm.Fill(hme_mass, mass);
-                hm.Fill(succ_rate, success_rate);
-
-                hm.Fill(res_mass_default, (HtoVV + bj1 + bj2).M());
-            }
+        hme = EstimateMass(input_offshell, pdf, rg, i);
+        if (hme)
+        {
+            auto [mass, eff] = hme.value();
+            hm.FillWeighted(hme_mass, mass, 0.5);
         }
     }
 
+    hm.Draw();
+
     auto end = std::chrono::system_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-    std::cout << "nEvents = " << nEvents << ", processing time = " << elapsed.count() << " s\n";
-
-    std::string level = USE_RECO ? "reco" : "gen";
-
-    hm.Draw();
-    hm.DrawStack({res_mass_default, hme_mass}, Form("X->HH: HME (gen H->WW & corr %s jet H->bb) vs default calculation", level.data()), "hme_vs_default.png");
-
     return 0;
 }
