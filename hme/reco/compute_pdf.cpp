e800e5826d7d2cd8dd4093a6605412f0a9c20786
abolshov <artem.e.bolshov@gmail.com>
2024-09-25 12:32:33 -0500

    add additional pdfs and scale pdfs to maximum bin content

--- hme/reco/compute_pdf.cpp
+++ hme/reco/compute_pdf.cpp
@@ -8,11 +8,19 @@
 #include "TFile.h"
 #include "TLorentzVector.h"
 #include "TH1.h"
+#include "TH2.h"
 #include "TChain.h"
 #include "TString.h"
-#include "TCanvas.h"
 
-static constexpr int N_RECO_JETS = 20;
+inline constexpr size_t N_RECO_JETS = 20;
+inline constexpr size_t N_BINS = 2000;
+
+template <class T>
+double GetPDFScaleFactor(std::unique_ptr<T> const& hist)
+{
+    int binmax = hist->GetMaximumBin();
+    return hist->GetBinContent(binmax);
+}
 
 int main()
 {
@@ -39,101 +47,64 @@ int main()
     Float_t         centralJet_phi[N_RECO_JETS];   
     Float_t         centralJet_mass[N_RECO_JETS]; 
 
-    // V2: hadronic
-    Double_t        genV2prod1_pt;
-    Double_t        genV2prod1_vis_pt;
-    Double_t        genV2prod1_eta;
-    Double_t        genV2prod1_vis_eta;
-    Double_t        genV2prod1_phi;
-    Double_t        genV2prod1_vis_phi;
-    Double_t        genV2prod1_mass;
-    Double_t        genV2prod1_vis_mass;
-
-    Double_t        genV2prod2_pt;
-    Double_t        genV2prod2_vis_pt;
-    Double_t        genV2prod2_eta;
-    Double_t        genV2prod2_vis_eta;
-    Double_t        genV2prod2_phi;
-    Double_t        genV2prod2_vis_phi;
-    Double_t        genV2prod2_mass;
-    Double_t        genV2prod2_vis_mass;
-
-    // H->bb
-    Double_t        genb1_pt;
-    Double_t        genb1_vis_pt;
-    Double_t        genb1_eta;
-    Double_t        genb1_vis_eta;
-    Double_t        genb1_phi;
-    Double_t        genb1_vis_phi;
-    Double_t        genb1_mass;
-    Double_t        genb1_vis_mass;
-    Double_t        genb2_pt;
-    Double_t        genb2_vis_pt;
-    Double_t        genb2_eta;
-    Double_t        genb2_vis_eta;
-    Double_t        genb2_phi;
-    Double_t        genb2_vis_phi;
-    Double_t        genb2_mass;
-    Double_t        genb2_vis_mass;
-
     chain->SetBranchAddress("ncentralJet", &ncentralJet);
     chain->SetBranchAddress("centralJet_pt", centralJet_pt);
     chain->SetBranchAddress("centralJet_eta", centralJet_eta);
     chain->SetBranchAddress("centralJet_phi", centralJet_phi);
     chain->SetBranchAddress("centralJet_mass", centralJet_mass);
 
-    chain->SetBranchAddress("genb1_pt", &genb1_pt);
-    chain->SetBranchAddress("genb1_eta", &genb1_eta);
-    chain->SetBranchAddress("genb1_phi", &genb1_phi);
-    chain->SetBranchAddress("genb1_mass", &genb1_mass);
+    // V2: hadronic
+    Double_t        genV2prod1_pt;
+    Double_t        genV2prod1_eta;
+    Double_t        genV2prod1_phi;
+    Double_t        genV2prod1_mass;
 
-    chain->SetBranchAddress("genb2_pt", &genb2_pt);
-    chain->SetBranchAddress("genb2_eta", &genb2_eta);
-    chain->SetBranchAddress("genb2_phi", &genb2_phi);
-    chain->SetBranchAddress("genb2_mass", &genb2_mass);
+    Double_t        genV2prod2_pt;
+    Double_t        genV2prod2_eta;
+    Double_t        genV2prod2_phi;
+    Double_t        genV2prod2_mass;
+
+    // H->bb
+    Double_t        genb1_pt;
+    Double_t        genb1_eta;
+    Double_t        genb1_phi;
+    Double_t        genb1_mass;
+
+    Double_t        genb2_pt;
+    Double_t        genb2_eta;
+    Double_t        genb2_phi;
+    Double_t        genb2_mass;
 
     chain->SetBranchAddress("genV2prod1_pt", &genV2prod1_pt);
-    chain->SetBranchAddress("genV2prod1_vis_pt", &genV2prod1_vis_pt);
     chain->SetBranchAddress("genV2prod1_eta", &genV2prod1_eta);
-    chain->SetBranchAddress("genV2prod1_vis_eta", &genV2prod1_vis_eta);
     chain->SetBranchAddress("genV2prod1_phi", &genV2prod1_phi);
-    chain->SetBranchAddress("genV2prod1_vis_phi", &genV2prod1_vis_phi);
     chain->SetBranchAddress("genV2prod1_mass", &genV2prod1_mass);
-    chain->SetBranchAddress("genV2prod1_vis_mass", &genV2prod1_vis_mass);
 
     chain->SetBranchAddress("genV2prod2_pt", &genV2prod2_pt);
-    chain->SetBranchAddress("genV2prod2_vis_pt", &genV2prod2_vis_pt);
     chain->SetBranchAddress("genV2prod2_eta", &genV2prod2_eta);
-    chain->SetBranchAddress("genV2prod2_vis_eta", &genV2prod2_vis_eta);
     chain->SetBranchAddress("genV2prod2_phi", &genV2prod2_phi);
-    chain->SetBranchAddress("genV2prod2_vis_phi", &genV2prod2_vis_phi);
     chain->SetBranchAddress("genV2prod2_mass", &genV2prod2_mass);
-    chain->SetBranchAddress("genV2prod2_vis_mass", &genV2prod2_vis_mass);
 
     chain->SetBranchAddress("genb1_pt", &genb1_pt);
-    chain->SetBranchAddress("genb1_vis_pt", &genb1_vis_pt);
     chain->SetBranchAddress("genb1_eta", &genb1_eta);
-    chain->SetBranchAddress("genb1_vis_eta", &genb1_vis_eta);
     chain->SetBranchAddress("genb1_phi", &genb1_phi);
-    chain->SetBranchAddress("genb1_vis_phi", &genb1_vis_phi);
     chain->SetBranchAddress("genb1_mass", &genb1_mass);
-    chain->SetBranchAddress("genb1_vis_mass", &genb1_vis_mass);
     chain->SetBranchAddress("genb2_pt", &genb2_pt);
-    chain->SetBranchAddress("genb2_vis_pt", &genb2_vis_pt);
     chain->SetBranchAddress("genb2_eta", &genb2_eta);
-    chain->SetBranchAddress("genb2_vis_eta", &genb2_vis_eta);
     chain->SetBranchAddress("genb2_phi", &genb2_phi);
-    chain->SetBranchAddress("genb2_vis_phi", &genb2_vis_phi);
     chain->SetBranchAddress("genb2_mass", &genb2_mass);
-    chain->SetBranchAddress("genb2_vis_mass", &genb2_vis_mass);
 
-    auto pdf = std::make_unique<TH1F>("pdf", "pdf", 1000, 0, 6);
+    auto pdf_b1 = std::make_unique<TH1F>("pdf_b1", "1d PDF for leading b jet correction", N_BINS, 0, 8);
+    auto pdf_b2 = std::make_unique<TH1F>("pdf_b2", "1d PDF for subleading b jet correction", N_BINS, 0, 8);
+    auto pdf_b1b2 = std::make_unique<TH2F>("pdf_b1b2", "2d PDF simultaneous b jet corrrections", N_BINS, 0, 8, N_BINS, 0, 8);
 
-    int nEvents = chain->GetEntries();
-    for (int i = 0; i < nEvents; ++i)
+    auto pdf_mbb = std::make_unique<TH1F>("pdf_mbb", "1d PDF of H->bb mass with true corrections applied", 5*N_BINS, 0, 200);
+
+    size_t nEvents = chain->GetEntries();
+    for (size_t i = 0; i < nEvents; ++i)
     {
         chain->GetEntry(i);
-        
+
         if (ncentralJet < 4)
         {
             continue;
@@ -145,25 +116,11 @@ int main()
         genq1_p4.SetPtEtaPhiM(genV2prod1_pt, genV2prod1_eta, genV2prod1_phi, genV2prod1_mass);
         genq2_p4.SetPtEtaPhiM(genV2prod2_pt, genV2prod2_eta, genV2prod2_phi, genV2prod2_mass);
 
-        // filter out resolved events
         if (genb1_p4.DeltaR(genb2_p4) < 0.4 || genq1_p4.DeltaR(genq2_p4) < 0.4)
         {
             continue;
         }
-
-        // require matching for all jets
-        bool all_matched = (genb1_vis_pt > 0.0) && (genb2_vis_pt > 0.0) && (genV2prod1_vis_pt > 0.0) && (genV2prod2_vis_pt); 
-        if (!all_matched)
-        {
-            continue;
-        }
-
-        // TLorentzVector gen_bj1_p4, gen_bj2_p4, gen_lj1_p4, gen_lj2_p4; // gen jets matched to quarks
-        // gen_bj1_p4.SetPtEtaPhiM(genb1_vis_pt, genb1_vis_eta, genb1_vis_phi, genb1_vis_mass);
-        // gen_bj2_p4.SetPtEtaPhiM(genb2_vis_pt, genb2_vis_eta, genb2_vis_phi, genb2_vis_mass);
-        // gen_lj1_p4.SetPtEtaPhiM(genV2prod1_vis_pt, genV2prod1_vis_eta, genV2prod1_vis_phi, genV2prod1_vis_mass);
-        // gen_lj2_p4.SetPtEtaPhiM(genV2prod2_vis_pt, genV2prod2_vis_eta, genV2prod2_vis_phi, genV2prod2_vis_mass);
-
+        
         // establish jet with greater pt for computing corrections
         // jets are sorted by btag so first two are b jets
         TLorentzVector reco_bj1_p4, reco_bj2_p4;
@@ -179,20 +136,24 @@ int main()
         }
 
         TLorentzVector lead_quark = genb1_p4.Pt() > genb2_p4.Pt() ? genb1_p4 : genb2_p4;
-        double r = lead_quark.Pt()/reco_bj1_p4.Pt();
-        pdf->Fill(r);
+        TLorentzVector subl_quark = genb1_p4.Pt() > genb2_p4.Pt() ? genb2_p4 : genb1_p4;
+        double c1 = lead_quark.Pt()/reco_bj1_p4.Pt();
+        double c2 = subl_quark.Pt()/reco_bj2_p4.Pt();
+        pdf_b1->Fill(c1);
+        pdf_b2->Fill(c1);
+        pdf_b1b2->Fill(c1, c2); 
+        pdf_mbb->Fill((c1*reco_bj1_p4 + c2*reco_bj2_p4).M());
     }
 
-    auto c1 = std::make_unique<TCanvas>("c1", "c1");
-    c1->SetGrid();
-    pdf->SetLineWidth(2);
-    pdf->Draw("hist");
-    pdf->Scale(1.0/pdf->Integral());
-
-    c1->SaveAs("pdf.png");
+    pdf_b1->Scale(1.0/GetPDFScaleFactor(pdf_b1));
+    pdf_b2->Scale(1.0/GetPDFScaleFactor(pdf_b2));
+    pdf_mbb->Scale(1.0/GetPDFScaleFactor(pdf_mbb));
 
     auto output = std::make_unique<TFile>("pdf.root","RECREATE");
-    pdf->Write();
+    pdf_b1->Write();
+    pdf_b2->Write();
+    pdf_mbb->Write();
+    pdf_b1b2->Write();
 	output->Write();
 	output->Close();
 
