a993f0147adff0e14c4c9661e2cff53f89e049fa
abolshov <artem.e.bolshov@gmail.com>
2024-10-18 11:57:29 -0500

    implement mappings to switch to dynamicly choosing gen truth storage

--- analyzer/Constants.hpp
+++ analyzer/Constants.hpp
@@ -25,19 +25,35 @@ enum class ObjDLRes { b1, b2, lep1, lep2, met, count };
 // used to index 4-vectors in the input of HME and 4-vectors in gen truth
 using Obj = std::conditional_t<N_LEP == 1, ObjSLRes, ObjDLRes>;
 
-// mapping Obj to corresponding gen truth branch names
-// static const std::map<ObjSLRes, char const*> GenTruthMapSL = { { ObjSLRes::b1, "genb1" },
-//                                                                { ObjSLRes::b2, "genb2" },
-//                                                                { ObjSLRes::q1, "genV2prod1" },
-//                                                                { ObjSLRes::q2, "genV2prod2" },
-//                                                                { ObjSLRes::lep, "genV1prod1" } };
+// obj name to its index in HME input (or index in gen truth)
+static const std::map<std::string, size_t> GenTruthIdxMapSL = { { "b1", 0 },
+                                                                { "b2", 1 },
+                                                                { "q1", 2 },
+                                                                { "q2", 3 },
+                                                                { "lep", 4 },
+                                                                { "met", 5 } };
 
-// static const std::map<ObjDLRes, char const*> GenTruthMapDL = { { ObjDLRes::b1, "genb1" },
-//                                                                { ObjDLRes::b2, "genb2" },
-//                                                                { ObjDLRes::lep1, "genV2prod1" },
-//                                                                { ObjDLRes::lep2, "genV2prod1" } };
+static const std::map<std::string, size_t> GenTruthIdxMapDL = { { "b1", 0 },
+                                                                { "b2", 1 },
+                                                                { "lep1", 2 },
+                                                                { "lep2", 3 },
+                                                                { "met", 4 } };
 
-// static const std::map<Obj, char const*> GenTruthMap = N_LEP == 1 ? GenTruthMapSL : GenTruthMapDL;
+// obj name to name of the branch with pt, eta, phi, mass of that object
+static const std::map<std::string, std::string> GenTruthBranchMapSL = { { "b1", "genb1" },
+                                                                        { "b2", "genb2" },
+                                                                        { "q1", "genV2prod1" },
+                                                                        { "q2", "genV2prod2" },
+                                                                        { "lep", "genV1prod1" },
+                                                                        { "met", "GenMET" } };
+
+static const std::map<std::string, std::string> GenTruthBranchMapDL = { { "b1", "genb1" },
+                                                                        { "b2", "genb2" },
+                                                                        { "lep1", "genV2prod1" },
+                                                                        { "lep2", "genV1prod1" },
+                                                                        { "met", "GenMET" } };
+
+static const std::vector<std::string> KinVarNames = { "_pt", "_eta", "_phi", "_mass" };
 
 // PDFs in SL channel resolved topology
 enum class PDF1dSLRes { pdf_b1, count };
