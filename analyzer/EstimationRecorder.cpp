0355a73b8d2b70014653b8794d8c589690e7f168
abolshov <artem.e.bolshov@gmail.com>
2025-03-07 15:59:12 -0600

    remove old files

--- analyzer/EstimationRecorder.cpp
+++
@@ -1,9 +0,0 @@
-#include "EstimationRecorder.hpp"
-
-EstimationRecorder::EstimationRecorder(TString const& dbg_file_name)
-{
-    if (!dbg_file_name.EqualTo(""))
-    {
-        m_file = std::make_unique<TFile>(dbg_file_name, "RECREATE");
-    }
-}
