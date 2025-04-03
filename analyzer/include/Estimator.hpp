f3cc3bb2b141575a2725682e1bb8f21b720ecdb4
abolshov <artem.e.bolshov@gmail.com>
2025-04-03 16:30:13 -0500

    add method to return estimator for given channel

--- analyzer/include/Estimator.hpp
+++ analyzer/include/Estimator.hpp
@@ -10,6 +10,7 @@ class Estimator
     Estimator(TString const& pdf_file_name_sl, TString const& pdf_file_name_dl, AggregationMode aggr_mode);
     OptArrF_t<ESTIM_OUT_SZ> EstimateMass(VecLVF_t const& jets, std::vector<Float_t> const& resolutions, VecLVF_t const& leptons, LorentzVectorF_t const& met, ULong64_t evt_id, Channel ch);
     void OpenDbgFile(TString const& dbg_file_name, Channel ch);
+    inline EstimatorBase& GetEstimator(Channel ch) { return ch == Channel::SL ? static_cast<EstimatorBase&>(m_estimator_sl) : static_cast<EstimatorBase&>(m_estimator_dl); }
 
     private:
     EstimatorSingleLep m_estimator_sl;
