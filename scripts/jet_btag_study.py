a20350f8341d7403516a3fd6662505a2bb5de80e
abolshov <artem.e.bolshov@gmail.com>
2025-04-14 14:10:40 -0500

    include plotting for SL channel

--- scripts/jet_btag_study.py
+++ scripts/jet_btag_study.py
@@ -5,62 +5,27 @@ import awkward as ak
 import matplotlib.pyplot as plt
 from CutTracker import CutTracker
 import os
+import itertools
 
 
-def MinDeltaRs(branches):
-    b1_p4 = vector.zip({'pt': branches['genb1_pt'],
-                       'eta': branches['genb1_eta'],
-                       'phi': branches['genb1_phi'],
-                       'mass': branches['genb1_mass']})
+def MaskIndices(indices, array_to_slice):
+    """
+    https://stackoverflow.com/questions/40894594/extract-elements-from-numpy-array-that-are-not-in-list-of-indexes
+    """
+    whole_set, in_set = ak.unzip(ak.cartesian([ak.local_index(array_to_slice), indices], nested=True))
+    return ak.any(whole_set == in_set, axis=-1)
 
-    b2_p4 = vector.zip({'pt': branches['genb2_pt'],
-                        'eta': branches['genb2_eta'],
-                        'phi': branches['genb2_phi'],
-                        'mass': branches['genb2_mass']})
 
-    pt = branches['centralJet_pt']
-    eta = branches['centralJet_eta']
-    phi = branches['centralJet_phi']
-    mass = branches['centralJet_mass']
-    jets = vector.zip({'pt': pt, 'eta': eta, 'phi': phi, 'mass': mass})
+def MinDeltaRs(branches, gen_branch_name):
+    parton1_p4 = vector.zip({'pt': branches[f'{gen_branch_name}1_pt'],
+                             'eta': branches[f'{gen_branch_name}1_eta'],
+                             'phi': branches[f'{gen_branch_name}1_phi'],
+                             'mass': branches[f'{gen_branch_name}1_mass']})
 
-    dr_b1 = ak.min(b1_p4.deltaR(jets), axis=1)
-    dr_b2 = ak.min(b2_p4.deltaR(jets), axis=1)
-    return dr_b1, dr_b2
-
-def HasMatching(branches):
-    dr_b1, dr_b2 = MinDeltaRs(branches)
-    return np.logical_and(dr_b1 < 0.4, dr_b2 < 0.4)
-
-def HasOneToOneMatching(branches):
-    b1_match_idx, b2_match_idx = MatchedJetIdx(branches)
-    return b1_match_idx != b2_match_idx
-
-def MaxMatchIdx(branches):
-    return np.maximum(*MatchedJetIdx(branches))
-
-def DeltaRBetweenBQuarks(branches):
-    b1_p4 = vector.zip({'pt': branches['genb1_pt'],
-                       'eta': branches['genb1_eta'],
-                       'phi': branches['genb1_phi'],
-                       'mass': branches['genb1_mass']})
-
-    b2_p4 = vector.zip({'pt': branches['genb2_pt'],
-                        'eta': branches['genb2_eta'],
-                        'phi': branches['genb2_phi'],
-                        'mass': branches['genb2_mass']})
-    return b1_p4.deltaR(b2_p4)
-
-def MatchedJetIdx(branches):
-    b1_p4 = vector.zip({'pt': branches['genb1_pt'],
-                       'eta': branches['genb1_eta'],
-                       'phi': branches['genb1_phi'],
-                       'mass': branches['genb1_mass']})
-
-    b2_p4 = vector.zip({'pt': branches['genb2_pt'],
-                        'eta': branches['genb2_eta'],
-                        'phi': branches['genb2_phi'],
-                        'mass': branches['genb2_mass']})
+    parton2_p4 = vector.zip({'pt': branches[f'{gen_branch_name}2_pt'],
+                             'eta': branches[f'{gen_branch_name}2_eta'],
+                             'phi': branches[f'{gen_branch_name}2_phi'],
+                             'mass': branches[f'{gen_branch_name}2_mass']})
 
     pt = branches['centralJet_pt']
     eta = branches['centralJet_eta']
@@ -68,13 +33,60 @@ def MatchedJetIdx(branches):
     mass = branches['centralJet_mass']
     jets = vector.zip({'pt': pt, 'eta': eta, 'phi': phi, 'mass': mass})
 
-    b1_match_idx = ak.argmin(b1_p4.deltaR(jets), axis=1)
-    b2_match_idx = ak.argmin(b2_p4.deltaR(jets), axis=1)
-    return b1_match_idx, b2_match_idx
+    dr1 = ak.min(parton1_p4.deltaR(jets), axis=1)
+    dr2 = ak.min(parton2_p4.deltaR(jets), axis=1)
+    return dr1, dr2
+
+def HasMatching(branches, gen_branch_name):
+    dr1, dr2 = MinDeltaRs(branches, gen_branch_name)
+    return np.logical_and(dr1 < 0.4, dr2 < 0.4)
+
+def HasOneToOneMatching(branches, gen_branch_name):
+    match1_idx, match_idx2 = MatchedJetIdx(branches, gen_branch_name)
+    return match1_idx != match_idx2
+
+def MaxMatchIdx(branches, gen_branch_name):
+    return np.maximum(*MatchedJetIdx(branches, gen_branch_name))
+
+def DeltaRBetweenQuarks(branches, gen_branch_name):
+    parton1_p4 = vector.zip({'pt': branches[f'{gen_branch_name}1_pt'],
+                             'eta': branches[f'{gen_branch_name}1_eta'],
+                             'phi': branches[f'{gen_branch_name}1_phi'],
+                             'mass': branches[f'{gen_branch_name}1_mass']})
+
+    parton2_p4 = vector.zip({'pt': branches[f'{gen_branch_name}2_pt'],
+                             'eta': branches[f'{gen_branch_name}2_eta'],
+                             'phi': branches[f'{gen_branch_name}2_phi'],
+                             'mass': branches[f'{gen_branch_name}2_mass']})
+
+    return parton1_p4.deltaR(parton2_p4)
+
+def MatchedJetIdx(branches, gen_branch_name):
+    parton1_p4 = vector.zip({'pt': branches[f'{gen_branch_name}1_pt'],
+                             'eta': branches[f'{gen_branch_name}1_eta'],
+                             'phi': branches[f'{gen_branch_name}1_phi'],
+                             'mass': branches[f'{gen_branch_name}1_mass']})
+
+    parton2_p4 = vector.zip({'pt': branches[f'{gen_branch_name}2_pt'],
+                             'eta': branches[f'{gen_branch_name}2_eta'],
+                             'phi': branches[f'{gen_branch_name}2_phi'],
+                             'mass': branches[f'{gen_branch_name}2_mass']})
+
+    pt = branches['centralJet_pt']
+    eta = branches['centralJet_eta']
+    phi = branches['centralJet_phi']
+    mass = branches['centralJet_mass']
+    jets = vector.zip({'pt': pt, 'eta': eta, 'phi': phi, 'mass': mass})
+
+    match1_idx = ak.argmin(parton1_p4.deltaR(jets), axis=1)
+    match2_idx = ak.argmin(parton2_p4.deltaR(jets), axis=1)
+    return match1_idx, match2_idx
 
 def main():
     file_name = "/Users/artembolshov/Desktop/CMS/Di-Higgs/data/GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-800/nano_0.root"
-    plotting_dir = "jet_btag_study"
+    # file_name = "/Users/artembolshov/Desktop/CMS/Di-Higgs/data/GluGlutoRadiontoHHto2B2Vto2B2JLNu_M-800/nano_0.root"
+    channel = "DL"
+    plotting_dir = f"jet_btag_study/{channel}"
     os.makedirs(plotting_dir, exist_ok=True)
 
     tracker = CutTracker(file_name)
@@ -82,60 +94,141 @@ def main():
     tracker.Apply(lambda branches: np.abs(branches['genb1_eta']) < 2.5, "b1 quark eta")
     tracker.Apply(lambda branches: branches['genb2_pt'] > 20, "b2 quark pt")
     tracker.Apply(lambda branches: np.abs(branches['genb2_eta']) < 2.5, "b2 quark eta")
-    tracker.Apply(lambda branches: branches['ncentralJet'] >= 3, "jet multiplicity")
+    if channel == "DL":
+        tracker.Apply(lambda branches: branches['ncentralJet'] >= 3, "jet multiplicity")
+    elif channel == "SL":
+        tracker.Apply(lambda branches: branches['ncentralJet'] >= 4, "jet multiplicity")
     tracker.Apply(lambda branches: branches['lep1_pt'] > 0, "has reco lep1")
-    tracker.Apply(lambda branches: branches['lep2_pt'] > 0, "has reco lep2")
-    tracker.Apply(lambda branches: DeltaRBetweenBQuarks(branches) > 0.4, "quarks dR")
-    tracker.Apply(lambda branches: HasMatching(branches), "matching")
-    tracker.Apply(lambda branches: HasOneToOneMatching(branches), "1 to 1 matching")
-    tracker.Apply(lambda branches: MaxMatchIdx(branches) == 2, "jet #3 is match")
-    # tracker.Apply(lambda branches: MaxMatchIdx(branches) < 2, "jets < #3 are matches")
+    if channel == "DL":
+        tracker.Apply(lambda branches: branches['lep2_pt'] > 0, "has reco lep2")
+    tracker.Apply(lambda branches: DeltaRBetweenQuarks(branches, "genb") > 0.4, "b quarks dR")
+    tracker.Apply(lambda branches: HasMatching(branches, "genb"), "b matching")
+    tracker.Apply(lambda branches: HasOneToOneMatching(branches, "genb"), "1 to 1 b matching")
+    # tracker.Apply(lambda branches: MaxMatchIdx(branches, "genb") == 2, "jet == 3 is match")
+    # tracker.Apply(lambda branches: MaxMatchIdx(branches, "genb") >= 2, "jet >= 3 is match")
+    # tracker.Apply(lambda branches: MaxMatchIdx(branches, "genb") < 2, "jets < #3 are matches")
+    
+    tagger_names = ["PNet", "DeepFlav"]
+    tagger_variables = {"PNet": ["B", "QvG", "CvL", "CvB"],
+                        "DeepFlav": ["B", "QG", "CvL", "CvB"]}
 
-    selected_branches = tracker.GetSelectedEvents()
-    btag_score = selected_branches["centralJet_btagPNetB"]
+    if channel == "DL":
+        print("this is DL channel")
 
-    tracker.Print()
+        tracker.PlotCutflow()
+        selected_branches = tracker.GetSelectedEvents()
 
-    # plot btag score of 3d jet when it is a match
-    plt.hist(btag_score[:, 2], bins=20, range=(0, 1), histtype='step')
-    plt.title("Jet #3 btag score")
-    plt.xlabel("btagPNetB score")
-    plt.ylabel("Count")
-    plt.grid(True)
-    plt.savefig(os.path.join(plotting_dir, "btag_3.pdf"), format='pdf', bbox_inches='tight')
-    plt.close()
+        m1, m2 = MatchedJetIdx(selected_branches, "genb")
 
-    plt.hist(btag_score[:, 2], bins=20, range=(0, 1), histtype='step', label="jet 3")
-    plt.hist(btag_score[:, 1], bins=20, range=(0, 1), histtype='step', label="jet 2")
-    plt.hist(btag_score[:, 0], bins=20, range=(0, 1), histtype='step', label="jet 1")
-    plt.title("Btag scores")
-    plt.xlabel("btagPNetB score")
-    plt.ylabel("Count")
-    plt.legend(loc='best')
-    plt.grid(True)
-    plt.savefig(os.path.join(plotting_dir, "btag_cmp.pdf"), format='pdf', bbox_inches='tight')
-    plt.close()
+        for tagger_name in tagger_names:
+            tagger_variable_names = tagger_variables[tagger_name]
+            for tagger_variable_name in tagger_variable_names:
+                score_array = selected_branches[f"centralJet_btag{tagger_name}{tagger_variable_name}"]
+                for num, match_jet_idx in enumerate([m1, m2]):    
+                    plt.hist(ak.flatten(score_array[ak.unflatten(match_jet_idx, 1)]), bins=20, range=(0, 1), histtype='step')
+                    plt.title(f"B jet #{num + 1} {tagger_name}{tagger_variable_name} score")
+                    plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                    plt.ylabel("Count")
+                    plt.grid(True)
+                    plt.savefig(os.path.join(plotting_dir, f"btag{tagger_name}{tagger_variable_name}_b{num + 1}.pdf"), format='pdf', bbox_inches='tight')
+                    plt.close()
 
-    plt.hist2d(btag_score[:, 2].to_numpy(), btag_score[:, 1].to_numpy(), bins=(10, 10), cmap=plt.cm.viridis, range=[[0, 1], [0, 1]])
-    plt.title("Btag scores")
-    plt.xlabel("jet 3 btag")
-    plt.ylabel("jet 2 btag")
-    plt.colorbar()
-    plt.savefig(os.path.join(plotting_dir, "btag3btag2.pdf"), format='pdf', bbox_inches='tight')
-    plt.close()
+                    plt.hist(score_array[:, 2], bins=20, range=(0, 1), histtype='step', label="jet 3")
+                    plt.hist(score_array[:, 1], bins=20, range=(0, 1), histtype='step', label="jet 2")
+                    plt.hist(score_array[:, 0], bins=20, range=(0, 1), histtype='step', label="jet 1")
+                    plt.title(f"{tagger_name}{tagger_variable_name} scores for first 3 jets")
+                    plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                    plt.ylabel("Count")
+                    plt.legend(loc='best')
+                    plt.grid(True)
+                    plt.savefig(os.path.join(plotting_dir, f"btag{tagger_name}{tagger_variable_name}_first_3.pdf"), format='pdf', bbox_inches='tight')
+                    plt.close()
 
-    plt.hist2d(btag_score[:, 2].to_numpy(), btag_score[:, 0].to_numpy(), bins=(10, 10), cmap=plt.cm.viridis, range=[[0, 1], [0, 1]])
-    plt.title("Btag scores")
-    plt.xlabel("jet 3 btag")
-    plt.ylabel("jet 1 btag")
-    plt.colorbar()
-    plt.savefig(os.path.join(plotting_dir, "btag3btag1.pdf"), format='pdf', bbox_inches='tight')
-    plt.close()
+                for jet1_idx, jet2_idx in itertools.combinations([0, 1, 2], 2):
+                    plt.hist2d(score_array[:, jet1_idx].to_numpy(), score_array[:, jet2_idx].to_numpy(), bins=(20, 20), cmap=plt.cm.viridis, range=[[0, 1], [0, 1]])
+                    plt.title(f"btag{tagger_name}{tagger_variable_name} scores")
+                    plt.xlabel(f"jet {jet1_idx + 1} btag{tagger_name}{tagger_variable_name} score")
+                    plt.ylabel(f"jet {jet2_idx + 1} btag{tagger_name}{tagger_variable_name} score")
+                    plt.colorbar()
+                    plt.savefig(os.path.join(plotting_dir, f"btag{tagger_name}{tagger_variable_name}_{jet1_idx + 1}vs{jet2_idx + 1}.pdf"), format='pdf', bbox_inches='tight')
+                    plt.close()
+        
+        # num_evts_medium_wp = ak.count_nonzero(btag_score[:, 2] > 0.5, axis=0)
+        # frac_med_wp = num_evts_medium_wp/len(btag_score[:, 2])*100
+        # print(f"Scenario: {tracker.GetLastCutDescription()}")
+        # print(f"Percentage of events with jet #3 passing medium btag WP: {frac_med_wp:.2f}%")
+    elif channel == "SL":
+        print("this is SL channel")
 
-    num_evts_medium_wp = ak.count_nonzero(btag_score[:, 2] > 0.5, axis=0)
-    frac_med_wp = num_evts_medium_wp/len(btag_score[:, 2])*100
-    print(f"Scenario: {tracker.GetLastCutDescription()}")
-    print(f"Percentage of events with jet #3 passing medium btag WP: {frac_med_wp:.2f}%")
+        tracker.Apply(lambda branches: branches['genV2prod1_pt'] > 20, "q1 pt")
+        tracker.Apply(lambda branches: np.abs(branches['genV2prod1_eta']) < 5.0, "q1 eta")
+        tracker.Apply(lambda branches: branches['genV2prod2_pt'] > 20, "q2 pt")
+        tracker.Apply(lambda branches: np.abs(branches['genV2prod2_eta']) < 5.0, "q2 eta")
+        tracker.Apply(lambda branches: DeltaRBetweenQuarks(branches, "genV2prod") > 0.4, "light quarks dR")
+        tracker.Apply(lambda branches: HasMatching(branches, "genV2prod"), "light matching")
+        tracker.Apply(lambda branches: HasOneToOneMatching(branches, "genV2prod"), "1 to 1 light matching")
+        tracker.PlotCutflow(percentages=True)
+        
+        selected_branches = tracker.GetSelectedEvents()
+        m1, m2 = MatchedJetIdx(selected_branches, "genV2prod")
+
+        for tagger_name in tagger_names:
+            tagger_variable_names = tagger_variables[tagger_name]
+            for tagger_variable_name in tagger_variable_names:
+                score_array = selected_branches[f"centralJet_btag{tagger_name}{tagger_variable_name}"]
+                for num, match_jet_idx in enumerate([m1, m2]):    
+                    plt.hist(ak.flatten(score_array[ak.unflatten(match_jet_idx, 1)]), bins=20, range=(0, 1), histtype='step')
+                    plt.title(f"light jet #{num + 1} {tagger_name}{tagger_variable_name} score")
+                    plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                    plt.ylabel("Count")
+                    plt.grid(True)
+                    plt.savefig(os.path.join(plotting_dir, f"btag{tagger_name}{tagger_variable_name}_q{num + 1}.pdf"), format='pdf', bbox_inches='tight')
+                    plt.close()
+
+                light_jet_indices = ak.concatenate([ak.unflatten(m1, 1), ak.unflatten(m2, 1)], axis=1)
+                light_jet_indices_mask = MaskIndices(light_jet_indices, score_array)
+
+                max_other_jets_score = ak.max(score_array[~light_jet_indices_mask], axis=1)
+
+                first_jet_score = ak.flatten(score_array[ak.unflatten(m1, 1)])
+                second_jet_score = ak.flatten(score_array[ak.unflatten(m2, 1)])
+                max_score = np.maximum(first_jet_score, second_jet_score)
+                min_score = np.minimum(first_jet_score, second_jet_score)
+
+                plt.hist(max_score, bins=20, range=(0, 1), histtype='step')
+                plt.title(f"Max light jet {tagger_name} {tagger_variable_name} score")
+                plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                plt.ylabel("Count")
+                plt.grid(True)
+                plt.savefig(os.path.join(plotting_dir, f"max_light_btag{tagger_name}{tagger_variable_name}.pdf"), format='pdf', bbox_inches='tight')
+                plt.close()
+
+                plt.hist(min_score, bins=20, range=(0, 1), histtype='step')
+                plt.title(f"Min light jet {tagger_name} {tagger_variable_name} score")
+                plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                plt.ylabel("Count")
+                plt.grid(True)
+                plt.savefig(os.path.join(plotting_dir, f"min_light_btag{tagger_name}{tagger_variable_name}.pdf"), format='pdf', bbox_inches='tight')
+                plt.close()
+
+                plt.hist(max_score, bins=20, range=(0, 1), histtype='step', label="max light")
+                plt.hist(max_other_jets_score, bins=20, range=(0, 1), histtype='step', label="max other")
+                plt.title(f"Max {tagger_name}{tagger_variable_name} score comparison")
+                plt.xlabel(f"btag{tagger_name}{tagger_variable_name} score")
+                plt.ylabel("Count")
+                plt.legend(loc="best")
+                plt.grid(True)
+                plt.savefig(os.path.join(plotting_dir, f"max_btag{tagger_name}{tagger_variable_name}_light_vs_other.pdf"), format='pdf', bbox_inches='tight')
+                plt.close()
+
+                plt.hist2d(max_score.to_numpy(), max_other_jets_score.to_numpy(), bins=(20, 20), cmap=plt.cm.viridis, range=[[0, 1], [0, 1]])
+                plt.title(f"Max {tagger_name}{tagger_variable_name} score")
+                plt.xlabel(f"Max light jet {tagger_name}{tagger_variable_name} score")
+                plt.ylabel(f"Max other jet {tagger_name}{tagger_variable_name} score")
+                plt.colorbar()
+                plt.savefig(os.path.join(plotting_dir, f"max_btag{tagger_name}{tagger_variable_name}_light_vs_other_2D.pdf"), format='pdf', bbox_inches='tight')
+                plt.close()
+
 
 if __name__ == '__main__':
     main()
