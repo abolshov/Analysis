c00afe2d36d25f044d4423d62ecbef0f064d32f4
abolshov <artem.e.bolshov@gmail.com>
2025-05-24 12:22:09 -0500

    add btag and qvg probabilities to jet combination

--- analyzer/include/JetCombination.hpp
+++ analyzer/include/JetCombination.hpp
@@ -7,11 +7,20 @@
 
 struct JetComb
 {
+    JetComb() = default;
+    JetComb(int b1_idx, int b2_idx, std::array<Float_t, MAX_RECO_JET> const& btag_score); // constructor for DL
+    JetComb(int b1_idx, int b2_idx, int q1_idx, int q2_idx,
+            std::array<Float_t, MAX_RECO_JET> const& btag_score,
+            std::array<Float_t, MAX_RECO_JET> const& qvg_score); // constructor for SL
+
     int b1 = -1;
     int b2 = -1;
     int q1 = -1;
     int q2 = -1;
+    Float_t btag_proba = 1.0f;
+    Float_t qvg_proba = 1.0f;
 
+    inline Float_t GetProbability() const { return btag_proba*qvg_proba; } 
     inline TString ToString(Channel ch) const
     {
         return ch == Channel::DL ? Form("b%db%d", b1, b2) : Form("b%db%dq%dq%d", b1, b2, q1, q2);
--- analyzer/src/JetCombination.cpp
+++ analyzer/src/JetCombination.cpp
@@ -1,5 +1,25 @@
 #include "JetCombination.hpp"
 
+JetComb::JetComb(int b1_idx, int b2_idx, std::array<Float_t, MAX_RECO_JET> const& btag_score)
+:   b1(b1_idx)
+,   b2(b2_idx)
+,   q1(-1)
+,   q2(-1)
+,   btag_proba(btag_score[b1_idx]*btag_score[b2_idx])
+,   qvg_proba(1.0)
+{}
+
+JetComb::JetComb(int b1_idx, int b2_idx, int q1_idx, int q2_idx,
+                 std::array<Float_t, MAX_RECO_JET> const& btag_score,
+                 std::array<Float_t, MAX_RECO_JET> const& qvg_score)
+:   b1(b1_idx)
+,   b2(b2_idx)
+,   q1(q1_idx)
+,   q2(q2_idx)
+,   btag_proba(btag_score[b1_idx]*btag_score[b2_idx])
+,   qvg_proba(qvg_score[q1_idx]*qvg_score[q2_idx])
+{}
+
 #ifdef DEV
     #include <unordered_set>
 
