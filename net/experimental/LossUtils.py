11de805545526327472e018a25436e4c28a55d3e
abolshov <artem.e.bolshov@gmail.com>
2025-07-01 09:37:03 -0500

    create separate utility module for loss functions

---
+++ net/experimental/LossUtils.py
@@ -0,0 +1,93 @@
+import tensorflow as tf 
+
+mh = tf.constant(125.0)
+
+# @tf.keras.saving.register_keras_serializable(package="CombinedLoss")
+class CombinedLoss(tf.keras.losses.Loss):
+    def __init__(self, strength=0.01, name="combined_loss", **kwargs):
+        super().__init__(name=name)
+        self.strength = strength
+        self.logcosh_loss = tf.keras.losses.LogCosh() 
+        self.mse_loss = tf.keras.losses.MeanSquaredError()
+
+    def call(self, y_true, y_pred):
+        logcosh = self.logcosh_loss(y_true, y_pred)
+        mbb2 = tf.square(y_pred[:, 3]) - tf.reduce_sum(tf.square(y_pred[:, 0:3]), axis=1, keepdims=True)
+        mww2 = tf.square(y_pred[:, 7]) - tf.reduce_sum(tf.square(y_pred[:, 4:7]), axis=1, keepdims=True)
+        mbb = tf.sign(mbb2)*tf.sqrt(tf.abs(mbb2))
+        mww = tf.sign(mww2)*tf.sqrt(tf.abs(mww2))
+        penalty = 0.5*self.mse_loss(mh, mbb) + 0.5*self.mse_loss(mh, mww)
+        total_loss = logcosh + self.strength*penalty
+        return total_loss 
+
+    def update_epoch(self):
+        self.current_epoch.assign_add(1)
+
+    def get_config(self):
+        """
+        Returns the serializable configuration of the layer.
+        This is necessary for saving and loading the model with custom objects.
+        """
+        config = super().get_config()
+        config.update({"strength": self.strength})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class MassLoss(tf.keras.losses.Loss):
+    def __init__(self, strength=1.0, name="mass_loss", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.mse_loss = tf.keras.losses.MeanSquaredError()
+        self.strength = strength
+
+    def call(self, y_true, y_pred):
+        mbb2 = tf.square(y_pred[:, 3]) - tf.reduce_sum(tf.square(y_pred[:, 0:3]), axis=1, keepdims=True)
+        mww2 = tf.square(y_pred[:, 7]) - tf.reduce_sum(tf.square(y_pred[:, 4:7]), axis=1, keepdims=True)
+        mbb = tf.sign(mbb2)*tf.sqrt(tf.abs(mbb2))
+        mww = tf.sign(mww2)*tf.sqrt(tf.abs(mww2))
+        loss = 0.5*self.mse_loss(mh, mbb) + 0.5*self.mse_loss(mh, mww)
+        return self.strength*loss 
+
+    def get_config(self):
+        """
+        Returns the serializable configuration of the layer.
+        This is necessary for saving and loading the model with custom objects.
+        """
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class Momentum3DLoss(tf.keras.losses.Loss):
+    def __init__(self, name="momentum3D_loss", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.loss_func = tf.keras.losses.LogCosh()
+
+    def call(self, y_true, y_pred):
+        pred_hbb_energy_sqr = tf.square(mh) + tf.reduce_sum(tf.square(y_pred[:, 0:3]), axis=1, keepdims=True)
+        pred_hvv_energy_sqr = tf.square(mh) + tf.reduce_sum(tf.square(y_pred[:, 3:5]), axis=1, keepdims=True)
+        pred_hbb_energy = tf.sign(pred_hbb_energy_sqr)*tf.sqrt(tf.abs(pred_hbb_energy_sqr))
+        pred_hvv_energy = tf.sign(pred_hvv_energy_sqr)*tf.sqrt(tf.abs(pred_hvv_energy_sqr))
+        # make sure format of labels is (px, py, pz, E)
+        energy_loss = self.loss_func(y_true[:, 3], pred_hbb_energy) + self.loss_func(y_true[:, 7], pred_hvv_energy)
+        momentum_loss = self.loss_func(y_true[:, 0:3], y_pred[:, 0:3]) + self.loss_func(y_true[:, 4:7], y_pred[:, 3:5])
+        total_loss = energy_loss + momentum_loss
+        return total_loss
+
+    def get_config(self):
+        """
+        Returns the serializable configuration of the layer.
+        This is necessary for saving and loading the model with custom objects.
+        """
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
