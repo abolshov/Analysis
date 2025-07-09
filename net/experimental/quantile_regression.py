4f5cb526fbc43f4abe90295df9937dd92dc80232
abolshov <artem.e.bolshov@gmail.com>
2025-07-09 10:50:03 -0500

    update script for quantile regression

--- net/experimental/quantile_regression.py
+++ net/experimental/quantile_regression.py
@@ -7,19 +7,19 @@ import os
 import matplotlib.pyplot as plt
 
 
-from DataWrapper import DataWrapper
+from Dataset import Dataset
 from NetUtils import TrainableSiLU, EpochLossUpdater
-from LossUtils import QuantileLoss, Momentum3DLoss
+from LossUtils import QuantileLoss
 
 
-target_names = {"genHbb_E": "E(H->bb)",
-                "genHbb_px": "px(H->bb)",
-                "genHbb_py": "py(H->bb)",
-                "genHbb_pz": "pz(H->bb)",
-                "genHVV_E": "E(H->VV)",
-                "genHVV_px": "px(H->VV)",
-                "genHVV_py": "py(H->VV)",
-                "genHVV_pz": "pz(H->VV)" }
+ground_truth_map = {"genHbb_E": "E(H->bb)",
+                    "genHbb_px": "px(H->bb)",
+                    "genHbb_py": "py(H->bb)",
+                    "genHbb_pz": "pz(H->bb)",
+                    "genHVV_E": "E(H->VV)",
+                    "genHVV_px": "px(H->VV)",
+                    "genHVV_py": "py(H->VV)",
+                    "genHVV_pz": "pz(H->VV)" }
 
 
 def PredWidth(pred_mass):
@@ -35,17 +35,16 @@ def PredPeak(pred_mass):
     return peak 
 
 
-def PlotCompare2D(target, output, quantity, plotting_dir=None):
+def PlotCompare2D(target, output, quantity, quantile, plotting_dir=None):
     plt.grid(False)
     min_bin = 0 if quantity[-1] == 'E' else -1200
     bins = np.linspace(min_bin, 1200, 100)
     plt.hist2d(target, output, bins=bins)
     var = quantity.split('_')[1]
     plt.title(f'{var} comparison')
-    quantile = quantity.split('_')[-1]
-    quantity = '_'.join(quantity.split('_')[:-1])
-    plt.ylabel(f'predicted {target_names[quantity]}')
-    plt.xlabel(f'true {target_names[quantity]}')
+    # quantity = '_'.join(quantity.split('_')[:-1])
+    plt.ylabel(f'predicted {ground_truth_map[quantity]}')
+    plt.xlabel(f'true {ground_truth_map[quantity]}')
     if plotting_dir:
         plt.savefig(os.path.join(plotting_dir, f"cmp2d_{quantity}_{quantile}.pdf"), bbox_inches='tight')
     else:
@@ -77,94 +76,103 @@ def PlotMetric(history, model, metric, plotting_dir=None):
     plt.clf()
 
 
-input_file_names = []
-with open("dl_train_files.txt", 'r') as file:
-    input_file_names = [line.rstrip() for line in file.readlines()]
-
-dw = DataWrapper()
-dw.ReadFiles(input_file_names)
-dw.AugmentDataset()
-dw.FormTrainSet()
-
-std = dw.label_scaler.scale_
-mean = dw.label_scaler.mean_
+dataset = Dataset('dataset_config.yaml')
+dataset.Load()
+X_train, input_names, y_train, target_names = dataset.Get(lambda df, mod, parity: df['event'] % mod == parity, 2, 0)
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 tf.random.set_seed(42)
 
-model_name = "model_hh_dl_aug_train_silu_l2reg_qloss"
+model_name = "model_hh_dl_train_silu_l2reg_qloss"
 model_dir = model_name
 os.makedirs(model_dir, exist_ok=True)
 
 learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(Scheduler)
-early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss',
-    patience=3,
-    min_delta=0.001
-)
 
-penalty_strength = tf.Variable(1e-6, dtype=tf.float32)
-update_rate = 1.0001
-loss_updater = EpochLossUpdater(penalty_strength, update_rate)
-
-input_shape = dw.train_features.shape
+input_shape = X_train.shape[1:]
 num_units = 384
 num_layers = 12
-dropout_rate = 0.05
 l2_strength = 0.01
 quantiles = [0.16, 0.5, 0.84]
-num_particles = 2
-num_components = 3
+num_quantiles = len(quantiles)
+epochs = 10
+batch_size = 512
 
-model = tf.keras.Sequential()
-for _ in range(num_layers):
-    model.add(tf.keras.layers.Dense(num_units, 
-                                    activation=TrainableSiLU(units=num_units), 
-                                    kernel_initializer='random_normal', 
-                                    bias_initializer='random_normal',
-                                    kernel_regularizer=tf.keras.regularizers.L2(l2_strength)))
-model.add(tf.keras.layers.Dense(num_components*num_particles*len(quantiles)))
+# prepare base part of the model
+inputs = tf.keras.layers.Input(shape=input_shape)
+x = tf.keras.layers.Dense(num_units, 
+                          activation=TrainableSiLU(units=num_units), 
+                          kernel_initializer='random_normal', 
+                          bias_initializer='random_normal',
+                          kernel_regularizer=tf.keras.regularizers.L2(l2_strength))(inputs)
+for _ in range(num_layers - 1):
+    x = tf.keras.layers.Dense(num_units, 
+                              activation=TrainableSiLU(units=num_units), 
+                              kernel_initializer='random_normal', 
+                              bias_initializer='random_normal',
+                              kernel_regularizer=tf.keras.regularizers.L2(l2_strength))(x)
 
-model.compile(loss=QuantileLoss(num_particles=num_particles, quantiles=quantiles), 
-              optimizer=tf.keras.optimizers.Adam(3e-4))
-model.build(dw.train_features.shape)
+# prepeare output heads
+num_targets = len(target_names)
+losses = {}
+outputs = []
+ys_train = []
+for idx, target_name in enumerate(target_names):
+    target_array = y_train[:, idx]
+    target_array = np.reshape(target_array, (-1, 1))
+    losses[target_name] = QuantileLoss(quantiles=quantiles, name=f'loss_{target_name}')
 
-history = model.fit(dw.train_features,
-                    dw.train_labels,
+    # repeat each target array num_quantiles times so that each output node has ground truth value
+    target_array = np.repeat(target_array, repeats=num_quantiles, axis=-1)
+    ys_train.append(target_array)
+
+    output = tf.keras.layers.Dense(num_quantiles, name=target_name)(x)
+    outputs.append(output)
+
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+model.compile(loss=losses, optimizer=tf.keras.optimizers.Adam(3e-4))
+
+history = model.fit(X_train,
+                    ys_train,
                     validation_split=0.2,
                     verbose=1,
-                    batch_size=2048,
-                    epochs=30,
+                    batch_size=batch_size,
+                    epochs=epochs,
                     callbacks=[learning_rate_scheduler])
 
 model.save(os.path.join(model_dir, f"{model_name}.keras"))
 PlotMetric(history, model_name, "loss", plotting_dir=model_dir)
 
-dw.Clear()
-dw.ReadFile("/Users/artembolshov/Desktop/CMS/Di-Higgs/data/GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-800/nano_0.root")
-# dw.ReadFile("/Users/artembolshov/Desktop/CMS/Di-Higgs/data/GluGlutoRadiontoHHto2B2Vto2B2JLNu_M-800/nano_0.root")
-dw.FormTestSet()
+# form testing dataset
+def TestSelection(df, mod, parity, mass):
+    return np.logical_and(df['event'] % mod == parity, df['X_mass'] == mass)
 
-res = model.predict(dw.test_features)
-ground_truth_names = ["genHbb_px", "genHbb_py", "genHbb_pz", "genHVV_px", "genHVV_py", "genHVV_pz"]
-pred_col_names = names = [f"{name}_q{int(100*q)}" for q in quantiles for name in ground_truth_names]
-pred_dict = {label: res[:, i] for i, label in enumerate(pred_col_names)}
+X_test, _, y_test, _ = dataset.Get(TestSelection, 2, 1, 800)
+ys_pred = model.predict(X_test)
+
+assert len(ys_pred) == len(target_names), f"mismatch between number of predicted values and ground truth values ({len(ys_pred)} vs {len(target_names)})"
+
+# ys_pred is a list of np arrays of shape (num_events, num_quantiles)
+pred_dict = {}
+for i, name in enumerate(target_names):
+    pred_array = ys_pred[i]
+    for q_idx, quantile in enumerate(quantiles):
+        q_pred_array = pred_array[:, q_idx]
+        q = int(100*quantile)
+        pred_dict[f'{name}_q{q}'] = q_pred_array
+
 pred_df = pd.DataFrame.from_dict(pred_dict)
+print(pred_df.head())
+print(pred_df.columns)
 
+bins = np.linspace(0, 2000, 100)
 for quantile in quantiles:
     q = int(100*quantile)
-    Hbb_en = np.sqrt(125.0**2 + pred_df[f"genHbb_px_q{q}"]**2 + pred_df[f"genHbb_py_q{q}"]**2 + pred_df[f"genHbb_pz_q{q}"]**2)
-    Hww_en = np.sqrt(125.0**2 + pred_df[f"genHVV_px_q{q}"]**2 + pred_df[f"genHVV_py_q{q}"]**2 + pred_df[f"genHVV_pz_q{q}"]**2)
-    X_en = Hbb_en + Hww_en
-    pred_df[f"genHbb_E_q{q}"] = Hbb_en
-    pred_df[f"genHVV_E_q{q}"] = Hww_en
 
-    for col in [col_name for col_name in list(pred_df.columns) if f"q{q}" in col_name]:
-        old_name = '_'.join(col.split('_')[:-1])
-        PlotCompare2D(dw.test_labels[old_name], pred_df[col], col, plotting_dir=model_dir)
-
-    bins = np.linspace(0, 2000, 100)
+    for i, col in enumerate(target_names):
+        ground_truth = y_test[:, i]
+        PlotCompare2D(ground_truth, pred_df[f'{col}_q{q}'], col, q, plotting_dir=model_dir)
 
     X_E_pred = pred_df[f"genHbb_E_q{q}"] + pred_df[f"genHVV_E_q{q}"]
     X_px_pred = pred_df[f"genHbb_px_q{q}"] + pred_df[f"genHVV_px_q{q}"]
