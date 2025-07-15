c55c7682213fee844ab3270e073722db27c2ff14
abolshov <artem.e.bolshov@gmail.com>
2025-07-15 12:17:58 -0500

    add some fixes for quantile order enforcement and error overestimation

--- net/experimental/LossUtils.py
+++ net/experimental/LossUtils.py
@@ -115,13 +115,39 @@ class QuantileLoss(tf.keras.losses.Loss):
     """
     Loss for predicting array of quantiles for single variable
     """
-    def __init__(self, quantiles=[0.5], name="quantile_loss", **kwargs):
+    def __init__(self, quantiles=[0.5], noncrossing=True, name="quantile_loss", **kwargs):
         super().__init__(name=name, **kwargs)
         self.quantiles = tf.constant(quantiles, dtype=tf.float32)
+        self.noncrossing = noncrossing
 
     def call(self, y_true, y_pred):
         error = y_true - y_pred
         loss = tf.reduce_mean(tf.maximum(self.quantiles*error, error*(self.quantiles - 1)), axis=-1)
+        
+        if self.noncrossing:
+            # # solution: https://github.com/scandido/quantile-regression-tf/blob/main/examples/Disallow_Crossing_Quantiles.ipynb
+            # def mask(x):
+            #     l = x.shape[-1]
+            #     k = x.shape[-1] // 2
+            #     m = tf.concat([-1*tf.ones(l - k - 1), tf.ones(l - k)], axis=0)
+            #     return m * x
+
+            # def sum_from_middle(x):
+            #     d = len(x.shape) - 1
+            #     knot = self.quantiles.shape[-1] // 2
+            #     a = tf.cumsum(x[..., :knot + 1], axis=d, reverse=True)
+            #     b = tf.cumsum(x[..., knot:], axis=d)
+            #     return tf.concat([a[..., :-1], b], axis=d)
+
+            # loss = mask(error)
+            # loss = sum_from_middle(loss)
+
+            quantile_gap = y_pred[:, 1:] - y_pred[:, :-1]
+            quantile_order_penalty = tf.reduce_mean(0.5*(1 - tf.sign(quantile_gap))*tf.abs(quantile_gap), axis=-1)
+            quantile_width_penalty = tf.abs(y_pred[:, 0] - y_pred[:, -1])
+            penalty = quantile_order_penalty + 0.1*quantile_width_penalty
+            return loss + penalty
+
         return loss
 
     def get_config(self):
@@ -130,7 +156,8 @@ class QuantileLoss(tf.keras.losses.Loss):
         This is necessary for saving and loading the model with custom objects.
         """
         config = super().get_config()
-        config.update({'quantiles': self.quantiles.numpy()})
+        config.update({'quantiles': self.quantiles.numpy(),
+                       'noncrossing': self.noncrossing})
         return config
 
     @classmethod
