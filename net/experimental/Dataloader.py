70cc88d69114be306bd0e1ad15ed72f8841dcb76
abolshov <artem.e.bolshov@gmail.com>
2025-07-17 13:08:28 -0500

    rename Dataset -> Dataloader and expand functionality

--- net/experimental/Dataloader.py
+++ net/experimental/Dataloader.py
@@ -6,6 +6,7 @@ import yaml
 import vector
 import matplotlib.pyplot as plt
 import os
+import time
 
 
 sample_type_map = {1: "GluGluToRadion",
@@ -20,23 +21,24 @@ def IsKinematic(var):
     return False
 
 
-class Dataset:
+class Dataloader:
     def __init__(self, branch_loading_cfg):
-        self.dataset_cfg = None
+        self.loader_cfg = None
         self.df = pd.DataFrame()
         self.p4_cache = {}
         with open (branch_loading_cfg, 'r') as branch_loading_cfg_file:
-            self.dataset_cfg = yaml.safe_load(branch_loading_cfg_file)
-        assert self.dataset_cfg, "Must contain mapping of branches to load for each object"
+            self.loader_cfg = yaml.safe_load(branch_loading_cfg_file)
+        assert self.loader_cfg, "Must contain mapping of branches to load for each object"
         
         self.input_names = self.MakeNameCollection('input_objects')
         self.target_names = self.MakeNameCollection('target_objects')
         
-        os.makedirs(self.dataset_cfg['plot_dir'], exist_ok=True)
+        if self.loader_cfg['plot_cutflow']:
+            os.makedirs(self.loader_cfg['plot_dir'], exist_ok=True)
 
     def MakeNameCollection(self, category):
         res = []
-        obj_cfg = self.dataset_cfg['objects'][category]
+        obj_cfg = self.loader_cfg['objects'][category]
         for obj_name, obj_params in obj_cfg.items():
             dim = obj_params['target_shape']
             features = [f for f in obj_params['branches_to_load'] if not IsKinematic(f)]
@@ -51,6 +53,32 @@ class Dataset:
         self.p4_cache.clear()
 
     def LoadObject(self, obj_name, obj_cfg, tree):
+        
+        object_present = len([bn for bn in tree.keys() if obj_name in bn]) > 1
+        if not object_present:
+            # fill dataframe with nans and return
+            n_events = tree.num_entries
+            nan_dict = {}
+            shape = obj_cfg['target_shape']
+            for var in obj_cfg['branches_to_load']:
+                if IsKinematic(var):
+                    continue
+                
+                if shape > 1:
+                    for i in range(shape):
+                        nan_dict[f'{obj_name}_{i + 1}_{var}'] = np.full(tree.num_entries, np.nan)
+                else:
+                    nan_dict[f'{obj_name}_{var}'] = np.full(tree.num_entries, np.nan)
+            
+            for var in obj_cfg['kinematics_output_format']:
+                if shape > 1:
+                    for i in range(shape):
+                        nan_dict[f'{obj_name}_{i + 1}_{var}'] = np.full(tree.num_entries, np.nan)
+                else:
+                    nan_dict[f'{obj_name}_{var}'] = np.full(tree.num_entries, np.nan)
+
+            return pd.DataFrame.from_dict(nan_dict)
+
         # only load in memory branches specified in the config
         arrays = tree.arrays([f'{obj_name}_{var}' for var in obj_cfg['branches_to_load']])
         target_shape = obj_cfg['target_shape']
@@ -98,33 +126,54 @@ class Dataset:
     def LoadObjects(self, tree):
         # concatenate data from all objects horizontally
         df = pd.DataFrame()
-        objects_to_load = self.dataset_cfg['objects']
+        objects_to_load = self.loader_cfg['objects']
         for obj_type, obj_type_settings in objects_to_load.items():
             for obj_name, cfg in obj_type_settings.items():
                 obj_df = self.LoadObject(obj_name, cfg, tree)
                 df = pd.concat([df, obj_df], axis=1)
         return df     
 
-    def Load(self):
-        for file_name in self.dataset_cfg['file_names']:
-            print(f'Loading {file_name}')
-            tree_name = self.dataset_cfg['tree_name']
-            tree = uproot.open(f'{file_name}:{tree_name}')
-            df = pd.concat([self.LoadObjects(tree), self.LoadBranches(tree)], axis=1)
+    def Load(self, loadable, append=True, apply_selections=None):
+        print(f'Loading data...')
+        start = time.perf_counter()
 
-            if self.dataset_cfg['apply_selections']:
-                df = self.ComputeCutflow(df)
-                df = self.ApplySelections(df, self.dataset_cfg['plot_cutflow'])
-            print(f'\tadding {len(df)} events')
-            self.df = pd.concat([self.df, df], axis=0)
-            self.ClearCache()
+        if apply_selections is None:
+            apply_selections = self.loader_cfg['apply_selections']
+
+        if isinstance(loadable, list):
+            for file_name in loadable:
+                self.LoadFile(file_name, append=append, apply_selections=apply_selections)
+        elif isinstance(loadable, str):
+                self.LoadFile(loadable, append=append, apply_selections=apply_selections)
+        else:
+            raise TypeError(f'Illegal type {type(loadable)} passed. Only str or list are allowed')
 
         # shuffle rows
         self.df = self.df.sample(frac=1, random_state=42).reset_index(drop=True)
+        end = time.perf_counter()
+        elapsed = end - start
+        print(f'Done in {elapsed:.2f}s')
+        print(f'Shape: {self.df.shape}')
+
+    def LoadFile(self, file_name, append=True, apply_selections=True):
+        tree_name = self.loader_cfg['tree_name']
+        tree = uproot.open(f'{file_name}:{tree_name}')
+        df = pd.concat([self.LoadObjects(tree), self.LoadBranches(tree)], axis=1)
+
+        if apply_selections:
+            df = self.ComputeCutflow(df)
+            df = self.ApplySelections(df, self.loader_cfg['plot_cutflow'])
+
+        if append:
+            self.df = pd.concat([self.df, df], axis=0)
+        else:
+            self.df = df
+
+        self.ClearCache()
     
     def LoadBranches(self, tree):
         aux_branches_dict = {}
-        branch_names = self.dataset_cfg['auxilliary_branches']
+        branch_names = self.loader_cfg['auxilliary_branches']
         for branch in branch_names:
             if branch in tree.keys():
                 aux_branches_dict[branch] = tree[branch].array().to_numpy()
@@ -184,7 +233,7 @@ class Dataset:
             df = df[df['b2_accept_eta']]
             cutflow['b2_accept_eta'] = len(df)
 
-            if self.dataset_cfg['use_topology'] == 'boosted':
+            if self.loader_cfg['use_topology'] == 'boosted':
                 df = df[df['is_boosted']]
                 cutflow['is_boosted'] = len(df)
                 df = df[df['reco_lep1']]
@@ -193,7 +242,7 @@ class Dataset:
                 cutflow['reco_lep2'] = len(df)
                 df = df[df['reco_as_boosted']]
                 cutflow['reco_as_boosted'] = len(df)
-            elif self.dataset_cfg['use_topology'] == 'resolved':
+            elif self.loader_cfg['use_topology'] == 'resolved':
                 df = df[df['is_resolved']]
                 cutflow['is_resolved'] = len(df)
                 df = df[df['reco_lep1']]
@@ -202,17 +251,17 @@ class Dataset:
                 cutflow['reco_lep2'] = len(df)
                 df = df[df['reco_as_resolved']]
                 cutflow['reco_as_resolved'] = len(df)
-            elif self.dataset_cfg['use_topology'] == 'mixed':
+            elif self.loader_cfg['use_topology'] == 'mixed':
                 df = df[df['successful_lep_reco']]
                 cutflow['successful_lep_reco'] = len(df)
                 df = df[df['successful_jet_reco']]
                 cutflow['successful_jet_reco'] = len(df)
             else:
-                raise RuntimeError(f"Got unknown topology {self.dataset_cfg['use_topology']}")
+                raise RuntimeError(f"Got unknown topology {self.loader_cfg['use_topology']}")
 
             cut_names = cutflow.keys()
             passed_evt_info = list(cutflow.values())
-            if self.dataset_cfg['use_percentages']:
+            if self.loader_cfg['use_percentages']:
                 passed_evt_info = [val/passed_evt_info[0]*100 for val in passed_evt_info]
             fig, ax = plt.subplots()
             bar_container = ax.bar(cut_names, passed_evt_info)
@@ -223,21 +272,23 @@ class Dataset:
             mass = int(mp) if not np.isnan(mp) else None
             title = f"Cutflow {sample_type} M={mass}" if mp else f"Cutflow {sample_type}"
             ax.set(ylabel='Number of events passed', title=title)
-            if self.dataset_cfg['use_percentages']:
+            if self.loader_cfg['use_percentages']:
                 ax.bar_label(bar_container, fmt=lambda x: f"{x:.1f}%", fontsize=8)
             else:
                 ax.bar_label(bar_container, fmt=lambda x: int(x), fontsize=8)
             plt.xticks(rotation=45, ha='right')
 
             cutlfow_name = f"cutflow_{sample_type}_M{mass}"
-            plt.savefig(os.path.join(self.dataset_cfg['plot_dir'], f"{cutlfow_name}.pdf"), format='pdf', bbox_inches='tight')
+            plt.savefig(os.path.join(self.loader_cfg['plot_dir'], f"{cutlfow_name}.pdf"), format='pdf', bbox_inches='tight')
             plt.close()
 
             return df
 
-    def Get(self, selection, *args, **kwargs):
+    def Get(self, selection=None, *args, **kwargs):
         """
         returns tuple (X, [names of features], y, [names of targets])
         """
-        df = self.df[selection(self.df, *args, **kwargs)]
-        return df[self.input_names].values, self.input_names, df[self.target_names].values, self.target_names
+        if selection:
+            df = self.df[selection(self.df, *args, **kwargs)]
+            return df[self.input_names].values, self.input_names, df[self.target_names].values, self.target_names
+        return self.df[self.input_names].values, self.input_names, self.df[self.target_names].values, self.target_names
