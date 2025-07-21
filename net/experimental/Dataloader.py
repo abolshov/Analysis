7fd6e179eaa06c9ccc1371cfbccf24fe73c3b570
abolshov <artem.e.bolshov@gmail.com>
2025-07-21 16:52:00 -0500

    make ApplySelections apply correct cuts to bkg samples

--- net/experimental/Dataloader.py
+++ net/experimental/Dataloader.py
@@ -11,8 +11,10 @@ import time
 
 sample_type_map = {1: "GluGluToRadion",
                    2: "GluGluToBulkGraviton", 
-                   3 : "VBFToRadion", 
-                   4 : "VBFToBulkGraviton"}
+                   3: "VBFToRadion", 
+                   4: "VBFToBulkGraviton",
+                   5: "DY",
+                   8: "TTbar"}
 
 
 def IsKinematic(var):
@@ -162,9 +164,8 @@ class Dataloader:
 
         if apply_selections:
             is_bkg = np.all(df['sample_type'] > 4)
-            df = self.ComputeCutflow(df, is_bkg)
-            if not is_bkg:
-                df = self.ApplySelections(df, self.loader_cfg['plot_cutflow'])
+            cuts_df = self.ComputeCutflow(df, is_bkg)
+            df = self.ApplySelections(df, cuts_df, self.loader_cfg['plot_cutflow'])
 
         if append:
             self.df = pd.concat([self.df, df], axis=0)
@@ -187,93 +188,89 @@ class Dataloader:
         cols_to_drop = [col for col in self.df.columns if col not in self.input_names + self.target_names]
         self.df.drop(cols_to_drop, axis=1, inplace=True)
 
-    def ComputeUniversalCuts(self, df, cuts):
-        # compute cuts applicable to both signal and bkg sample
-        cuts['reco_lep1'] = np.logical_and(self.p4_cache['lep1'].pt > 0.0, df['lep1_legType'] == df['lep1_gen_kind'])
-        cuts['reco_lep2'] = np.logical_and(self.p4_cache['lep2'].pt > 0.0, df['lep2_legType'] == df['lep2_gen_kind'])
-        cuts['successful_lep_reco'] = np.logical_and(cuts['reco_lep1'], cuts['reco_lep2'])
+    def ComputeGenLevelCuts(self, df, cuts):
+        # compute signal specific cuts on gen level objects in order they will be applied
+        b1_accept_pt = self.p4_cache['genb1'].pt > 20
+        b1_accept_eta = np.abs(self.p4_cache['genb1'].eta) < 2.5
+        cuts['b1_accept'] = np.logical_and(b1_accept_pt, b1_accept_eta)
+        
+        b2_accept_pt = self.p4_cache['genb2'].pt > 20
+        b2_accept_eta = np.abs(self.p4_cache['genb2'].eta) < 2.5
+        cuts['b2_accept'] = np.logical_and(b2_accept_pt, b2_accept_eta)
+
+        cuts['lep_accept'] = np.logical_and(df['genV1prod1_pt'] > 5.0, df['genV2prod1_pt'] > 5.0)
         return cuts
 
-    def ComputeSignalCuts(self, df, cuts):
-        # compute signal specific cuts
-        cuts['b1_accept_pt'] = self.p4_cache['genb1'].pt > 20
-        cuts['b2_accept_pt'] = self.p4_cache['genb2'].pt > 20
-        cuts['b1_accept_eta'] = np.abs(self.p4_cache['genb1'].eta) < 2.5
-        cuts['b2_accept_eta'] = np.abs(self.p4_cache['genb2'].eta) < 2.5
-        cuts['genb1_accept'] = np.logical_and(cuts['b1_accept_pt'], cuts['b1_accept_eta'])
-        cuts['genb2_accept'] = np.logical_and(cuts['b2_accept_pt'], cuts['b2_accept_eta'])
-        cuts['genb_accept'] = np.logical_and(cuts['genb1_accept'], cuts['genb2_accept'])
-        cuts['bb_dr'] = self.p4_cache['genb1'].deltaR(self.p4_cache['genb2'])
-        cuts['mindR_b1_Jet'] = ak.min(self.p4_cache['genb1'].deltaR(self.p4_cache['centralJet']), axis=1)
-        cuts['mindR_b2_Jet'] = ak.min(self.p4_cache['genb2'].deltaR(self.p4_cache['centralJet']), axis=1)
-        cuts['b1_match_idx'] = ak.argmin(self.p4_cache['genb1'].deltaR(self.p4_cache['centralJet']), axis=1)
-        cuts['b2_match_idx'] = ak.argmin(self.p4_cache['genb2'].deltaR(self.p4_cache['centralJet']), axis=1)
-        cuts['has_two_matches'] = np.logical_and(cuts['mindR_b1_Jet'] < 0.4, cuts['mindR_b2_Jet'] < 0.4)
-        cuts['mindR_Hbb_FatJet'] = ak.min(self.p4_cache['genHbb'].deltaR(self.p4_cache['SelectedFatJet']), axis=1)
-        cuts['is_boosted'] = cuts['bb_dr'] < 0.8
-        cuts['is_resolved'] = ~cuts['is_boosted']
-        cuts['reco_as_boosted'] = np.logical_and(cuts['mindR_Hbb_FatJet'] < 0.8, df['nSelectedFatJet'] >= 1)
-        cuts['reco_2b'] = np.logical_and(cuts['has_two_matches'], cuts['b1_match_idx'] != cuts['b2_match_idx'])
-        cuts['reco_as_resolved'] = np.logical_and(cuts['reco_2b'], df['ncentralJet'] >= 2)
-        cuts['successful_jet_reco'] = np.logical_or(cuts['reco_as_resolved'], cuts['reco_as_boosted'])
-        cuts['successful_reco'] = np.logical_and(cuts['successful_lep_reco'], cuts['successful_jet_reco'])
+    def ComputeRecoLevelCuts(self, df, cuts):
+        # compute signal specific cuts on gen level objects in order they will be applied
+        ak4_selection = np.logical_and(self.p4_cache['centralJet'].pt > 20.0, np.abs(self.p4_cache['centralJet'].eta) < 2.5)
+        has_ak4_jets = ak.count_nonzero(ak4_selection, axis=1) >= 2
+        ak8_selection = np.logical_and(self.p4_cache['SelectedFatJet'].pt > 200.0, np.abs(self.p4_cache['SelectedFatJet'].eta) < 2.5)
+        has_ak8_jets = ak.count_nonzero(ak8_selection, axis=1) >= 1
+        cuts['has_reco_jets'] = np.logical_or(has_ak8_jets, has_ak4_jets)
+        cuts['has_reco_leptons'] = np.logical_and(self.p4_cache['lep1'].pt > 0.0, self.p4_cache['lep2'].pt > 0.0)
+        return cuts
+
+    def ComputeGenRecoMatchingCuts(self, df, cuts):
+        topology = self.loader_cfg['use_topology']
+        bb_dr = self.p4_cache['genb1'].deltaR(self.p4_cache['genb2'])
+        is_boosted = bb_dr < 0.8
+
+        mindR_b1_Jet = ak.min(self.p4_cache['genb1'].deltaR(self.p4_cache['centralJet']), axis=1)
+        mindR_b2_Jet = ak.min(self.p4_cache['genb2'].deltaR(self.p4_cache['centralJet']), axis=1)
+        b1_match_idx = ak.argmin(self.p4_cache['genb1'].deltaR(self.p4_cache['centralJet']), axis=1)
+        b2_match_idx = ak.argmin(self.p4_cache['genb2'].deltaR(self.p4_cache['centralJet']), axis=1)
+        has_two_matches = np.logical_and(mindR_b1_Jet < 0.4, mindR_b2_Jet < 0.4)
+        reco_2b = np.logical_and(has_two_matches, b1_match_idx != b2_match_idx)
+
+        mindR_Hbb_FatJet = ak.min(self.p4_cache['genHbb'].deltaR(self.p4_cache['SelectedFatJet']), axis=1)
+
+        match topology:
+            case 'resolved':
+                cuts['is_resolved'] = ~is_boosted
+                cuts['reco_as_resolved'] = np.logical_and(reco_2b, df['ncentralJet'] >= 2)
+            case 'boosted':
+                cuts['is_boosted'] = is_boosted
+                cuts['reco_as_boosted'] = np.logical_and(mindR_Hbb_FatJet < 0.8, df['nSelectedFatJet'] >= 1)
+            case 'mixed':
+                as_fat = np.logical_and(mindR_Hbb_FatJet < 0.8, df['nSelectedFatJet'] >= 1)
+                as_slim = np.logical_and(reco_2b, df['ncentralJet'] >= 2)
+                cuts['successful_jet_reco'] = np.logical_or(as_fat, as_slim)
+            case _:
+                raise RuntimeError(f'Illegal use_topology value: {topology}')
+
+        # lepton matching
+        reco_lep1 = df['lep1_legType'] == df['lep1_gen_kind']
+        reco_lep2 = df['lep2_legType'] == df['lep2_gen_kind']
+        cuts['successful_lep_reco'] = np.logical_and(reco_lep1, reco_lep2)
         return cuts
 
     def ComputeCutflow(self, df, is_bkg):
         # I want it to return list of cuts it computed in certain order for plotting cutflow
         cut_dict = {}
-        cut_dict = self.ComputeUniversalCuts(df, cut_dict)
-        
         if not is_bkg:
-            cut_dict.update(self.ComputeSignalCuts(df, cut_dict))
+            cut_dict = self.ComputeGenLevelCuts(df, cut_dict)
+        
+        cut_dict.update(self.ComputeRecoLevelCuts(df, cut_dict))
+
+        if not is_bkg:
+            cut_dict.update(self.ComputeGenRecoMatchingCuts(df, cut_dict))
     
         cut_df = pd.DataFrame.from_dict(cut_dict)
-        df = pd.concat([df, cut_df], axis=1)
-        return df
+        return cut_df
 
-    def ApplySelections(self, df, plot_cutflow):
-        # I want to rewrite this one as loop over cuts to be applied
-        if not plot_cutflow:
-            df = df[df['successful_reco']]
-            return df
-        else:
-            cutflow = {}
-            cutflow['total'] = len(df)
-            df = df[df['b1_accept_pt']]
-            cutflow['b1_accept_pt'] = len(df)
-            df = df[df['b1_accept_eta']]
-            cutflow['b1_accept_eta'] = len(df)
-            df = df[df['b2_accept_pt']]
-            cutflow['b2_accept_pt'] = len(df)
-            df = df[df['b2_accept_eta']]
-            cutflow['b2_accept_eta'] = len(df)
+    def ApplySelections(self, df, cut_df, plot_cutflow):
+        cutflow = {}
+        valid = np.full(len(cut_df), True)
+        cutflow['total'] = len(cut_df)
+        for cut_name in cut_df.columns:
+            cut = cut_df[cut_name]
+            valid = np.logical_and(cut, valid)
+            cutflow[cut_name] = np.sum(valid)
 
-            if self.loader_cfg['use_topology'] == 'boosted':
-                df = df[df['is_boosted']]
-                cutflow['is_boosted'] = len(df)
-                df = df[df['reco_lep1']]
-                cutflow['reco_lep1'] = len(df)
-                df = df[df['reco_lep2']]
-                cutflow['reco_lep2'] = len(df)
-                df = df[df['reco_as_boosted']]
-                cutflow['reco_as_boosted'] = len(df)
-            elif self.loader_cfg['use_topology'] == 'resolved':
-                df = df[df['is_resolved']]
-                cutflow['is_resolved'] = len(df)
-                df = df[df['reco_lep1']]
-                cutflow['reco_lep1'] = len(df)
-                df = df[df['reco_lep2']]
-                cutflow['reco_lep2'] = len(df)
-                df = df[df['reco_as_resolved']]
-                cutflow['reco_as_resolved'] = len(df)
-            elif self.loader_cfg['use_topology'] == 'mixed':
-                df = df[df['successful_lep_reco']]
-                cutflow['successful_lep_reco'] = len(df)
-                df = df[df['successful_jet_reco']]
-                cutflow['successful_jet_reco'] = len(df)
-            else:
-                raise RuntimeError(f"Got unknown topology {self.loader_cfg['use_topology']}")
+        df = df[valid]
 
+        if plot_cutflow:        
             cut_names = cutflow.keys()
             passed_evt_info = list(cutflow.values())
             if self.loader_cfg['use_percentages']:
@@ -281,11 +278,10 @@ class Dataloader:
             fig, ax = plt.subplots()
             bar_container = ax.bar(cut_names, passed_evt_info)
 
-            # print(df.describe())
             sample_type = sample_type_map[df['sample_type'].iloc[0]]
             mp = df['X_mass'].iloc[0]
             mass = int(mp) if not np.isnan(mp) else None
-            title = f"Cutflow {sample_type} M={mass}" if mp else f"Cutflow {sample_type}"
+            title = f"Cutflow {sample_type} M={mass}" if mp and mp > 0 else f"Cutflow {sample_type}"
             ax.set(ylabel='Number of events passed', title=title)
             if self.loader_cfg['use_percentages']:
                 ax.bar_label(bar_container, fmt=lambda x: f"{x:.1f}%", fontsize=8)
@@ -293,11 +289,13 @@ class Dataloader:
                 ax.bar_label(bar_container, fmt=lambda x: int(x), fontsize=8)
             plt.xticks(rotation=45, ha='right')
 
-            cutlfow_name = f"cutflow_{sample_type}_M{mass}"
+            cutlfow_name = f"cutflow_{sample_type}"
+            if mp > 0:
+                cutlfow_name += f'_{mp}'
             plt.savefig(os.path.join(self.loader_cfg['plot_dir'], f"{cutlfow_name}.pdf"), format='pdf', bbox_inches='tight')
             plt.close()
 
-            return df
+        return df
 
     def Get(self, selection=None, *args, **kwargs):
         """
