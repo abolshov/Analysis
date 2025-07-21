6f195b7c722038ce6c13a0f71bfcd9cbe3cd91c1
abolshov <artem.e.bolshov@gmail.com>
2025-07-21 12:46:43 -0500

    remove space

--- net/experimental/ModelUtils.py
+++ net/experimental/ModelUtils.py
@@ -18,7 +18,7 @@ def LoadModel(cfg_file_name):
         module_mapping = cfg['custom_objects']
         for object_name, module_name in module_mapping.items():  
             module = importlib.import_module(module_name)
-            custom_objects[object_name] =  getattr(module, object_name)
+            custom_objects[object_name] = getattr(module, object_name)
 
     path_to_model = os.path.join(model_dir, f'{model_name}.{model_fmt}')
     model = tf.keras.models.load_model(path_to_model, custom_objects=custom_objects)
