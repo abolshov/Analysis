a773b219c005967787d9997f0eace0757c9cb0e8
abolshov <artem.e.bolshov@gmail.com>
2025-07-26 10:34:07 -0500

    add utility library; add string concatenation tools as a workaround while c++20 is not available

---
+++ analysis/Utils/include/Utils.hpp
@@ -0,0 +1,49 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+
+//--- functions to calculate the total size ---
+size_t StrSize(const char* str) 
+{
+    return strlen(str);
+}
+
+size_t StrSize(const std::string& str) 
+{
+    return str.size();
+}
+
+template <class Head, class... Tail>
+size_t StrSize(const Head& head, Tail const&... tail) 
+{
+    return StrSize(head) + StrSize(tail...);
+}
+
+//--- functions to append strings together ---
+template <class Head>
+void StrAppend(std::string& out, const Head& head) 
+{
+    out += head;
+}
+
+template <class Head, class... Args>
+void StrAppend(std::string& out, const Head& head, Args const&... args) 
+{
+    out += head;
+    StrAppend(out, args...);
+}
+
+//--- Finally, the function to concatenate strings ---
+template <class... Args> 
+std::string StrCat(Args const&... args) 
+{
+    size_t tot_size = StrSize(args...);
+    std::string out;
+    out.reserve(tot_size);
+
+    StrAppend(out, args...);
+    return out;
+}
+
+#endif
