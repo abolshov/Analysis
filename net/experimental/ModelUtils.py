381a5fe399c374c6e35709035ad1bb7ac67d5b00
abolshov <artem.e.bolshov@gmail.com>
2025-07-30 10:39:22 -0500

    implement conversion to onnx

--- net/experimental/ModelUtils.py
+++ net/experimental/ModelUtils.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import yaml
 import importlib
 import os
+import onnx
+import tf2onnx
 
 
 def LoadModel(cfg_file_name):
@@ -23,3 +25,15 @@ def LoadModel(cfg_file_name):
     path_to_model = os.path.join(model_dir, f'{model_name}.{model_fmt}')
     model = tf.keras.models.load_model(path_to_model, custom_objects=custom_objects)
     return model
+
+def ConvertToONNX(model, input_signature, out_path=None, op_set=13):
+    """
+    input_signature ~ [tf.TensorSpec([None, 157], tf.float32, name='input')]
+    """
+
+    onnx_model, _ = tf2onnx.convert.from_keras(model,
+                                               input_signature=input_signature,
+                                               opset=op_set)
+    if out_path:
+        onnx.save(onnx_model, os.path.join(out_path, f'{model.name}.onnx'))
+    return onnx_model
