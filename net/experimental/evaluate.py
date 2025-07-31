57bdec668968215e47bdce2f887303acbce2f2fb
abolshov <artem.e.bolshov@gmail.com>
2025-07-31 08:52:00 -0500

    add script for evaluation of pretrained model

--- net/experimental/evaluate.py
+++ net/experimental/evaluate.py
@@ -2,90 +2,141 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import os
-
+import yaml
+from sklearn.preprocessing import StandardScaler
 
 from Dataloader import Dataloader
-from NetUtils import *
-from LossUtils import *
-from LayerUtils import *
-
 
 from MiscUtils import *
-from PlotUtils import PlotHist
+from PlotUtils import PlotHist, PlotCompare2D
+from ModelUtils import LoadModel
 
 
 def main():
     # load dataset for model evaluation
-    file = 'DY.root'
+    # file = 'DY.root'
+    file = '../train_data/Run3_2022/GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M_800/nano_0.root'
     dataloader = Dataloader('dataloader_config.yaml')
-    dataloader.Load(file, apply_selections=False)
-    X, input_names, y, target_names = dataloader.Get()
+    dataloader.Load(file)
+
+    def TestSelection(df, mod, parity, mass, sample_type):
+        tmp = np.logical_and(df['event'] % mod == parity, df['X_mass'] == mass)
+        return np.logical_and(tmp, df['sample_type'] == sample_type)
+        
+    X, input_names, y, target_names = dataloader.Get(TestSelection, 2, 1, 800, 1)
 
     # load model
-    model_dir = 'model_hh_dl_train_silu_qloss_wwidth_weights_l2reg'
-    model_name = 'model_hh_dl_train_silu_qloss_wwidth_weights_l2reg'
-    path_to_model = os.path.join(model_dir, f'{model_name}.keras')
-    custom_objects = {'TrainableSiLU': TrainableSiLU,
-                      'QuantileLoss': QuantileLoss,
-                      'QuantileOrderingLayer': QuantileOrderingLayer}
-    model = tf.keras.models.load_model(path_to_model, custom_objects=custom_objects)
+    model, training_params = LoadModel('model_cfg.yaml')
+    print(model.summary())
 
     # make predictions and plot them
-    ys_pred = model.predict(X)
-    
-    pred_dict = {}
-    quantiles = [0.16, 0.5, 0.84]
-    for i, name in enumerate(target_names):
-        pred_array = ys_pred[i]
-        for q_idx, quantile in enumerate(quantiles):
-            q_pred_array = pred_array[:, q_idx]
-            q = int(100*quantile)
-            pred_dict[f'{name}_q{q}'] = q_pred_array
+    ys_pred = None
+    if training_params['standardize']:
+        input_means = training_params['input_train_means']
+        input_scales = training_params['input_train_scales']
+        X -= input_means
+        X /= input_scales
+        ys_pred = model.predict(X)
 
-    pred_df = pd.DataFrame.from_dict(pred_dict)
+        pred = np.array(ys_pred[:-1])
+        pred = np.squeeze(pred[:, :, 1]).T
+        pred *= training_params['target_train_scales']
+        pred += training_params['target_train_means']
 
-    bins = np.linspace(0, 2000, 100)
-    for quantile in quantiles:
-        q = int(100*quantile)
+        ys_pred = pred
 
-        X_E_pred = pred_df[f"genHbb_E_q{q}"] + pred_df[f"genHVV_E_q{q}"]
-        X_px_pred = pred_df[f"genHbb_px_q{q}"] + pred_df[f"genHVV_px_q{q}"]
-        X_py_pred = pred_df[f"genHbb_py_q{q}"] + pred_df[f"genHVV_py_q{q}"]
-        X_pz_pred = pred_df[f"genHbb_pz_q{q}"] + pred_df[f"genHVV_pz_q{q}"]
+        # for i, arr in enumerate(ys_pred[:-1] if training_params['add_mass_loss'] else ys_pred):
+        #     if training_params['quantiles']:
+        #         for q in range(training_params['num_quantiles']):
+        #             arr[:, q] *= training_params['target_train_scales'][i]
+        #             arr[:, q] += training_params['target_train_means'][i]
+        #         else:
+        #             arr *= training_params['target_train_scales'][i]
+        #             arr += training_params['target_train_means'][i]
+    else:
+        ys_pred = model.predict(X)
 
-        X_mass_pred_sqr = X_E_pred**2 - X_px_pred**2 - X_py_pred**2 - X_pz_pred**2
-        X_mass_sqr_pos = X_mass_pred_sqr[X_mass_pred_sqr > 0.0]
-        print(f"quantile {q}: positive mass fraction: {len(X_mass_sqr_pos)}/{len(X_mass_pred_sqr)}={len(X_mass_pred_sqr[X_mass_pred_sqr > 0.0])/len(X_mass_pred_sqr):.3f}")
+   
+    hvv_PtEtaPhiE_pred = ToPtEtaPhiE(ys_pred[:, :4])
+    hvv_PtEtaPhiE_true = ToPtEtaPhiE(y[:, :4])
 
-        X_mass_pred = np.sqrt(X_mass_pred_sqr[X_mass_pred_sqr > 0.0])
-        PlotHist(data=X_mass_pred, 
-                 bins=bins,
-                 title="Predicted X->HH mass",
-                 ylabel='Count',
-                 xlabel='Mass, [GeV]',
-                 plotting_dir=model_dir,
-                 peak=True,
-                 width=True,
-                 count=True,
-                 file_name=f'eval_mass_q{q}')
+    hbb_PtEtaPhiE_pred = ToPtEtaPhiE(ys_pred[:, 4:])
+    hbb_PtEtaPhiE_true = ToPtEtaPhiE(y[:, 4:])
 
-    # plot errors
-    for i, name in enumerate(target_names):
-        up = pred_df[f'{name}_q84']
-        down = pred_df[f'{name}_q16']
-        err = up - down
-        pred = pred_df[f'{name}_q50']
-        print(f'Target {name}:')
-        print(f'\tquantile crossing fraction: {len(err[err < 0.0])/len(err):.2f}')
+    PtEtaPhiE_pred = np.concatenate([hvv_PtEtaPhiE_pred, hbb_PtEtaPhiE_pred], axis=-1)
+    PtEtaPhiE_true = np.concatenate([hvv_PtEtaPhiE_true, hbb_PtEtaPhiE_true], axis=-1)
 
-        bins = np.linspace(np.min(err) - 10, np.max(err) + 10, 50)
-        PlotHist(data=err, 
-                 bins=bins,
-                 title=f'Predicted {name} error',
-                 ylabel='Count',
-                 xlabel='Error, [GeV]',
-                 plotting_dir=model_dir,
-                 file_name=f'eval_error_{name}')
+    var_names = ['genHVV_pt', 'genHVV_eta', 'genHVV_phi', 'genHVV_E', 
+                 'genHbb_pt', 'genHbb_eta', 'genHbb_phi', 'genHbb_E']
+    for i, name in enumerate(var_names):
+        var = name.split('_')[-1]
+        obj = name.split('_')[0]
+
+        bin_left = np.min([np.quantile(PtEtaPhiE_true[:, i], 0.01), np.quantile(PtEtaPhiE_pred[:, i], 0.01)]) - 1.0
+        bin_right = np.max([np.quantile(PtEtaPhiE_true[:, i], 0.99), np.quantile(PtEtaPhiE_pred[:, i], 0.99)]) + 1.0
+        bins = np.linspace(bin_left, bin_right, 100)
+
+        PlotCompare2D(PtEtaPhiE_true[:, i], 
+                      PtEtaPhiE_pred[:, i], 
+                      var,
+                      obj,
+                      bins=bins, 
+                      title=f'{objects[obj] if obj in objects else obj} {pretty_vars[var] if var in pretty_vars else var} comparison',
+                      xlabel=f'True {ground_truth_map[name]}',
+                      ylabel=f'Predicted {ground_truth_map[name]}',
+                      plotting_dir=os.path.join(training_params['model_dir'], 'plots'))
+
+    # pred_dict = {}
+    # quantiles = [0.16, 0.5, 0.84]
+    # for i, name in enumerate(target_names):
+    #     pred_array = ys_pred[i]
+    #     for q_idx, quantile in enumerate(quantiles):
+    #         q_pred_array = pred_array[:, q_idx]
+    #         q = int(100*quantile)
+    #         pred_dict[f'{name}_q{q}'] = q_pred_array
+
+    # bins = np.linspace(0, 2000, 100)
+    # for quantile in quantiles:
+    #     q = int(100*quantile)
+
+    #     X_E_pred = pred_df[f"genHbb_E_q{q}"] + pred_df[f"genHVV_E_q{q}"]
+    #     X_px_pred = pred_df[f"genHbb_px_q{q}"] + pred_df[f"genHVV_px_q{q}"]
+    #     X_py_pred = pred_df[f"genHbb_py_q{q}"] + pred_df[f"genHVV_py_q{q}"]
+    #     X_pz_pred = pred_df[f"genHbb_pz_q{q}"] + pred_df[f"genHVV_pz_q{q}"]
+
+    #     X_mass_pred_sqr = X_E_pred**2 - X_px_pred**2 - X_py_pred**2 - X_pz_pred**2
+    #     X_mass_sqr_pos = X_mass_pred_sqr[X_mass_pred_sqr > 0.0]
+    #     print(f"quantile {q}: positive mass fraction: {len(X_mass_sqr_pos)}/{len(X_mass_pred_sqr)}={len(X_mass_pred_sqr[X_mass_pred_sqr > 0.0])/len(X_mass_pred_sqr):.3f}")
+
+    #     X_mass_pred = np.sqrt(X_mass_pred_sqr[X_mass_pred_sqr > 0.0])
+    #     PlotHist(data=X_mass_pred, 
+    #              bins=bins,
+    #              title="Predicted X->HH mass",
+    #              ylabel='Count',
+    #              xlabel='Mass, [GeV]',
+    #              plotting_dir=model_dir,
+    #              peak=True,
+    #              width=True,
+    #              count=True,
+    #              file_name=f'eval_mass_q{q}')
+
+    # # plot errors
+    # for i, name in enumerate(target_names):
+    #     up = pred_df[f'{name}_q84']
+    #     down = pred_df[f'{name}_q16']
+    #     err = up - down
+    #     pred = pred_df[f'{name}_q50']
+    #     print(f'Target {name}:')
+    #     print(f'\tquantile crossing fraction: {len(err[err < 0.0])/len(err):.2f}')
+
+    #     bins = np.linspace(np.min(err) - 10, np.max(err) + 10, 50)
+    #     PlotHist(data=err, 
+    #              bins=bins,
+    #              title=f'Predicted {name} error',
+    #              ylabel='Count',
+    #              xlabel='Error, [GeV]',
+    #              plotting_dir=model_dir,
+    #              file_name=f'eval_error_{name}')
 
 
 if __name__ == '__main__':
