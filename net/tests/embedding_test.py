ba0e5df40ceab62748850f662cd42bf499559c60
abolshov <artem.e.bolshov@gmail.com>
2025-08-11 15:20:17 -0500

    update embedding code

--- net/tests/embedding_test.py
+++ net/tests/embedding_test.py
@@ -8,39 +8,30 @@ from experimental.LayerUtils import Embedding, ParticleEmbedding
 
 def main():
     lep_tensor = tf.random.normal(shape=(10, 8))
-    lep_embedding = Embedding(num_layers=1, 
-                              num_units=512,
-                              dim_embedding=512)
+    lep_embedding = Embedding(dim_embedding=512, dropout_rate=0.01)
     lep_out = lep_embedding(lep_tensor)
     print(f'Lepton embedding output shape: {lep_out.shape}')
     print(f'Lepton embedding param count: {lep_embedding.count_params()}')
 
     jet_tensor = tf.random.normal(shape=(10, 120))
-    jet_embedding = Embedding(num_layers=1, 
-                              num_units=512,
-                              dim_embedding=512)
+    jet_embedding = Embedding(dim_embedding=512, dropout_rate=0.01)
     jet_out = jet_embedding(jet_tensor)
     print(f'Jet embedding output shape: {jet_out.shape}')
     print(f'Jet embedding param count: {jet_embedding.count_params()}')
 
     fatjet_tensor = tf.random.normal(shape=(10, 27))
-    fatjet_embedding = Embedding(num_layers=1, 
-                              num_units=512,
-                              dim_embedding=512)
+    fatjet_embedding = Embedding(dim_embedding=512, dropout_rate=0.01)
     fatjet_out = fatjet_embedding(fatjet_tensor)
     print(f'Fatjet embedding output shape: {fatjet_out.shape}')
     print(f'Fatjet embedding param count: {fatjet_embedding.count_params()}')
 
     met_tensor = tf.random.normal(shape=(10, 2))
-    met_embedding = Embedding(num_layers=1, 
-                              num_units=512,
-                              dim_embedding=512)
+    met_embedding = Embedding(dim_embedding=512, dropout_rate=0.01)
     met_out = met_embedding(met_tensor)
     print(f'MET embedding output shape: {met_out.shape}')
     print(f'MET embedding param count: {met_embedding.count_params()}')
 
-    particle_embedding = ParticleEmbedding(num_layers=1, num_units=512, dim_embedding=512)
-    # particle_tensor = tf.concat([lep_tensor, met_tensor, jet_tensor, fatjet_tensor], axis=1)
+    particle_embedding = ParticleEmbedding(dim_embedding=512, dropout_rate=0.01)
     particle_out = particle_embedding([lep_tensor, met_tensor, jet_tensor, fatjet_tensor])
     print(f'Particle embedding output shape: {particle_out.shape}')
     print(f'Particle embedding param count: {particle_embedding.count_params()}')
