3838feba087bb57bfe5cd76ee5dcd6fff913b8df
abolshov <artem.e.bolshov@gmail.com>
2025-08-12 10:23:54 -0500

    update encoder test

--- net/tests/encoder_test.py
+++ net/tests/encoder_test.py
@@ -1,31 +1,20 @@
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
 import tensorflow as tf
-from LayerUtils import Encoder, Embedding
-
-def encoder_test():
-    print('Encoder test')
-    t = tf.random.normal(shape=(10, 157))
-    encoder = Encoder(num_encoder_layers=6, 
-                      num_proj_layers=2,
-                      dim_proj_layer=512,
-                      d_model=512,
-                      num_heads=8, 
-                      dff=2048,
-                      dropout_rate=0.02)
-    out = encoder(t)
-    print(f'Encoder output shape: {out.shape}')
-
-def embedding_test():
-    print('Embedding test')
-    t = tf.random.normal(shape=(10, 157))
-    embedding = Embedding(num_layers=3, 
-                          dim_layer=512,
-                          dim_embedding=512)
-    out = embedding(t)
-    print(f'Embedding output shape: {out.shape}')
+from experimental.LayerUtils import Encoder
 
 def main():
-    encoder_test()
-    embedding_test()
+    encoder = Encoder(num_encoder_layers=6, d_model=128, num_heads=8, dff=512)
+    lep_tensor = tf.random.normal(shape=(10, 8))
+    met_tensor = tf.random.normal(shape=(10, 2))
+    jet_tensor = tf.random.normal(shape=(10, 120))
+    fatjet_tensor = tf.random.normal(shape=(10, 27))
+    encoder_output = encoder([lep_tensor, met_tensor, jet_tensor, fatjet_tensor])
+    print(f'Encoder output shape: {encoder_output.shape}')
+    print(f'Encoder param count: {encoder.count_params()}')
 
 if __name__ == '__main__':
     main()
