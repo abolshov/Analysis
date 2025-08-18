f98abc926fd6f78fef5550840b7515d7e5c136a0
abolshov <artem.e.bolshov@gmail.com>
2025-08-18 10:37:36 -0500

    add new encoder test

--- net/tests/encoder_test.py
+++ net/tests/encoder_test.py
@@ -7,14 +7,20 @@ import tensorflow as tf
 from experimental.LayerUtils import Encoder
 
 def main():
-    encoder = Encoder(num_encoder_layers=6, d_model=128, num_heads=8, dff=512)
-    lep_tensor = tf.random.normal(shape=(10, 8))
-    met_tensor = tf.random.normal(shape=(10, 2))
-    jet_tensor = tf.random.normal(shape=(10, 120))
-    fatjet_tensor = tf.random.normal(shape=(10, 27))
-    encoder_output = encoder([lep_tensor, met_tensor, jet_tensor, fatjet_tensor])
-    print(f'Encoder output shape: {encoder_output.shape}')
+    input_mapping = [1]*2 + [2] + [3]*10 + [4]*3
+
+    encoder = Encoder(num_encoder_layers=6, d_model=64, num_heads=8, dff=256, input_mapping=input_mapping)
+
+    lep_inputs = [tf.random.normal(shape=(10, 4)) for _ in range(2)]
+    met_inputs = [tf.random.normal(shape=(10, 2)) for _ in range(1)]
+    jet_inputs = [tf.random.normal(shape=(10, 12)) for _ in range(10)]
+    fatjet_inputs = [tf.random.normal(shape=(10, 9)) for _ in range(3)]
+    inputs = lep_inputs + met_inputs + jet_inputs + fatjet_inputs
+
+    output = encoder(inputs)
+    print(f'Encoder output shape: {output.shape}')
     print(f'Encoder param count: {encoder.count_params()}')
+    print(encoder.compute_output_shape(inputs))
 
 if __name__ == '__main__':
     main()
