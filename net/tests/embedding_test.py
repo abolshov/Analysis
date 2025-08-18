b342860e3ec3b150eacf3baf44d7f67f5115e64b
abolshov <artem.e.bolshov@gmail.com>
2025-08-18 10:46:41 -0500

    update embedding test

--- net/tests/embedding_test.py
+++ net/tests/embedding_test.py
@@ -4,37 +4,21 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import tensorflow as tf
-from experimental.LayerUtils import Embedding, EmbeddingLayer
+from experimental.LayerUtils import Embedding
 
 def main():
-    lep_tensor = tf.random.normal(shape=(10, 8))
-    lep_embedding = EmbeddingLayer(dim_embedding=512, dropout_rate=0.01)
-    lep_out = lep_embedding(lep_tensor)
-    print(f'Lepton embedding output shape: {lep_out.shape}')
-    print(f'Lepton embedding param count: {lep_embedding.count_params()}')
+    input_mapping = [1]*2 + [2] + [3]*10 + [4]*3
+    embedding = Embedding(dim_embedding=64, dropout_rate=0.01, input_mapping=input_mapping)
 
-    jet_tensor = tf.random.normal(shape=(10, 120))
-    jet_embedding = EmbeddingLayer(dim_embedding=512, dropout_rate=0.01)
-    jet_out = jet_embedding(jet_tensor)
-    print(f'Jet embedding output shape: {jet_out.shape}')
-    print(f'Jet embedding param count: {jet_embedding.count_params()}')
+    lep_inputs = [tf.random.normal(shape=(10, 4)) for _ in range(2)]
+    met_inputs = [tf.random.normal(shape=(10, 2)) for _ in range(1)]
+    jet_inputs = [tf.random.normal(shape=(10, 12)) for _ in range(10)]
+    fatjet_inputs = [tf.random.normal(shape=(10, 9)) for _ in range(3)]
+    inputs = lep_inputs + met_inputs + jet_inputs + fatjet_inputs
 
-    fatjet_tensor = tf.random.normal(shape=(10, 27))
-    fatjet_embedding = EmbeddingLayer(dim_embedding=512, dropout_rate=0.01)
-    fatjet_out = fatjet_embedding(fatjet_tensor)
-    print(f'Fatjet embedding output shape: {fatjet_out.shape}')
-    print(f'Fatjet embedding param count: {fatjet_embedding.count_params()}')
-
-    met_tensor = tf.random.normal(shape=(10, 2))
-    met_embedding = EmbeddingLayer(dim_embedding=512, dropout_rate=0.01)
-    met_out = met_embedding(met_tensor)
-    print(f'MET embedding output shape: {met_out.shape}')
-    print(f'MET embedding param count: {met_embedding.count_params()}')
-
-    particle_embedding = Embedding(dim_embedding=512, dropout_rate=0.01)
-    particle_out = particle_embedding([lep_tensor, met_tensor, jet_tensor, fatjet_tensor])
-    print(f'Particle embedding output shape: {particle_out.shape}')
-    print(f'Particle embedding param count: {particle_embedding.count_params()}')
+    output = embedding(inputs)
+    print(f'Particle embedding output shape: {output.shape}')
+    print(f'Particle embedding param count: {embedding.count_params()}')
 
 if __name__ == '__main__':
     main()
