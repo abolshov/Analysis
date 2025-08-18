76d0a0d95259a553bb574c71f33ef907e03f82a0
abolshov <artem.e.bolshov@gmail.com>
2025-08-18 13:24:33 -0500

    fix passing inputs to encoder

--- net/experimental/transformer_regression.py
+++ net/experimental/transformer_regression.py
@@ -24,14 +24,14 @@ def main():
 
     dataloader = Dataloader('dataloader_config.yaml')
     # dataloader.Load(files)
-    dataloader.Load('../nano_0.root')
+    dataloader.Load('../train_data/Run3_2022/GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M_800/nano_0.root')
     input_objects = dataloader.GetObjData(objects=['lep1', 'lep2', 'met', 'centralJet', 'SelectedFatJet'], 
                                           as_df=False,
                                           unravel=True,
                                           selector=lambda df: df['event'] % 2 == 0)
 
     print('Inputs:')
-    for obj, (names, arr) in inputs.items():
+    for obj, (names, arr) in input_objects.items():
         print(f'\t{obj}: {arr.shape}')
 
     target_objects = dataloader.GetObjData(objects=['genHbb', 'genHVV'], 
@@ -40,28 +40,34 @@ def main():
                                            selector=lambda df: df['event'] % 2 == 0)
 
     print('Targets:')
-    for obj, (names, arr) in targets.items():
+    for obj, (names, arr) in target_objects.items():
         print(f'\t{obj}: {arr.shape}')
 
     # prepare model inputs
-    lep_inputs = [inputs['lep1'][1], inputs['lep2'][1]]
-    met_inputs = [inputs['met'][1]]
-    jet_inputs = [inputs[name][1] for name in inputs.keys() if 'centralJet' in name]
-    fatjet_inputs = [inputs[name][1] for name in inputs.keys() if 'SelectedFatJet' in name]
+    lep_inputs = [input_objects['lep1'][1], input_objects['lep2'][1]]
+    met_inputs = [input_objects['met'][1]]
+    jet_inputs = [input_objects[name][1] for name in input_objects.keys() if 'centralJet' in name]
+    fatjet_inputs = [input_objects[name][1] for name in input_objects.keys() if 'SelectedFatJet' in name]
     inputs = lep_inputs + met_inputs + jet_inputs + fatjet_inputs
 
     # prepare input layer for the model
     # each particle will have its own input layer
     # then all layers will be concatenated to take list of particles as model input
-    lep_input_layer = tf.keras.layers.Input(shape=lep_inputs[0].shape[1:])
-    met_input_layer = tf.keras.layers.Input(shape=met_inputs[0].shape[1:])
-    jet_input_layer = tf.keras.layers.Input(shape=jet_inputs[0].shape[1:])
-    fatjet_input_layer = tf.keras.layers.Input(shape=fatjet_inputs[0].shape[1:])
-    model_input_layer = tf.layers.layer.Concatenate([lep_input_layer]*2 + [met_input_layer] + [jet_input_layer]*10 + [fatjet_input_layer]*3)
+    lep_input_layer = tf.keras.layers.Input(shape=lep_inputs[0].shape[1:], name='leptons')
+    met_input_layer = tf.keras.layers.Input(shape=met_inputs[0].shape[1:], name='MET')
+    jet_input_layer = tf.keras.layers.Input(shape=jet_inputs[0].shape[1:], name='AK4Jets')
+    fatjet_input_layer = tf.keras.layers.Input(shape=fatjet_inputs[0].shape[1:], name='AK8Jets')
+
+    model_input_layers = [lep_input_layer]*2 + [met_input_layer] + [jet_input_layer]*10 + [fatjet_input_layer]*3
+    model_input_layer = tf.keras.layers.Concatenate(axis=1)(model_input_layers)
 
     input_mapping = [1]*2 + [2] + [3]*10 + [4]*3
-    encoder = Encoder(num_encoder_layers=6, d_model=64, num_heads=8, dff=256, input_mapping=input_mapping)
-    
+    encoder = Encoder(num_encoder_layers=8, d_model=64, num_heads=8, dff=256, input_mapping=input_mapping)
+        
+    x = encoder(model_input_layers)
+    outputs = tf.keras.layers.Flatten()(x)
+    model = tf.keras.Model(inputs=model_input_layers, outputs=outputs, name='transformer')
+    print(model.summary())
 
 if __name__ == '__main__':
     main()
