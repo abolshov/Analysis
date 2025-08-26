0f05fd914dd4393bb40fc6e7da0c4d817ba60eeb
abolshov <artem.e.bolshov@gmail.com>
2025-08-26 15:17:54 -0500

    cleanup onnx concersion script and adjust behaviour of LoadModel when training cfg is missing

--- net/experimental/ModelUtils.py
+++ net/experimental/ModelUtils.py
@@ -4,9 +4,15 @@ import importlib
 import os
 import onnx
 import tf2onnx
+import sys
 
 
-def LoadModel(cfg_file_name):
+def LoadModel(cfg_file_name, allow_missing_train_cfg=True):
+    """
+        function for loading .keras model
+        returns: tuple (model, training_parameters)
+        if allow_missing_train_cfg is True, training_parameters will be None if .yaml with is not found, otherwise exception will be re-thrown
+    """
     cfg = None
     with open(cfg_file_name, 'r') as cfg_file:
         cfg = yaml.safe_load(cfg_file)
@@ -27,18 +33,26 @@ def LoadModel(cfg_file_name):
 
     # load training params if available
     training_params = None
-    with open(os.path.join(model_dir, 'params.yaml'), 'r') as train_cfg_file:
-        training_params = yaml.safe_load(train_cfg_file)
+    training_cfg_name = f'params_{model_name}.yaml'
+    traing_cfg_path = os.path.join(model_dir, training_cfg_name)
+    try:
+        with open(traing_cfg_path, 'r') as train_cfg_file:
+            training_params = yaml.safe_load(train_cfg_file)
+    except FileNotFoundError:
+        print(f'Model training config file {training_cfg_name} not found in {model_dir}')
+        if not allow_missing_train_cfg:
+            raise
     return model, training_params
 
-def ConvertToONNX(model, input_signature, out_path=None, op_set=13):
+def ConvertToONNX(model, input_signature, save_to=None, op_set=13):
     """
     input_signature ~ [tf.TensorSpec([None, 157], tf.float32, name='input')]
+    if save_to is None, model is not saved
     """
 
     onnx_model, _ = tf2onnx.convert.from_keras(model,
                                                input_signature=input_signature,
                                                opset=op_set)
-    if out_path:
-        onnx.save(onnx_model, os.path.join(out_path, f'{model.name}.onnx'))
+    if save_to:
+        onnx.save(onnx_model, os.path.join(save_to, f'{model.name}.onnx'))
     return onnx_model
--- net/experimental/to_onnx.py
+++ net/experimental/to_onnx.py
@@ -1,32 +1,19 @@
 import tensorflow as tf
 import tf2onnx
 import onnx
-# from onnx_tf.backend import prepare
 
 from ModelUtils import LoadModel, ConvertToONNX
 
 def main():
-    model, _ = LoadModel('model_cfg.yaml')
-    # print(model.name)
-    print(model.summary())
+    keras_model, training_params = LoadModel('model_cfg.yaml', allow_missing_train_cfg=False)
+    print(keras_model.summary())
 
     input_signature = [tf.TensorSpec([None, 157], tf.float32, name='input')]
-    onnx_model = ConvertToONNX(model, input_signature, 'conversion_test')
-    # onnx_model, _ = tf2onnx.convert.from_keras(model,
-    #                                            input_signature=input_signature,
-    #                                            opset=13)
+    onnx_model = ConvertToONNX(keras_model, input_signature, out_path='predict_quantiles3D_DL_v4')
 
-    # onnx.save(onnx_model, 'conversion_test/model.onnx')
-
-    onnx_model = onnx.load('conversion_test/model.onnx')
     onnx.checker.check_model(onnx_model)
-    # print(onnx.helper.printable_graph(onnx_model.graph))
+    print(onnx.helper.printable_graph(onnx_model.graph))
 
-    # tf_rep = prepare(onnx_model)
-    # tf_rep.export_graph('tf_model')
-
-    # tf_model = tf.saved_model.load('tf_model')
-    # infer = model.signatures['seving_default']
 
 if __name__ == '__main__':
     main()
