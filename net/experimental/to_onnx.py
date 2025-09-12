1678e20ded9df909786ff05ec7d79f18aa1225b5
abolshov <artem.e.bolshov@gmail.com>
2025-09-12 09:27:37 -0500

    read model parameters from training config in onnx converter

--- net/experimental/to_onnx.py
+++ net/experimental/to_onnx.py
@@ -8,8 +8,10 @@ def main():
     keras_model, training_params = LoadModel('model_cfg.yaml', allow_missing_train_cfg=False)
     print(keras_model.summary())
 
-    input_signature = [tf.TensorSpec([None, 157], tf.float32, name='input')]
-    onnx_model = ConvertToONNX(keras_model, input_signature, out_path='predict_quantiles3D_DL_v4')
+    num_features = training_params['input_shape'][0]
+    input_signature = [tf.TensorSpec([None, num_features], tf.float32, name='input')]
+    model_dir = training_params['model_dir']
+    onnx_model = ConvertToONNX(keras_model, input_signature, model_dir)
 
     onnx.checker.check_model(onnx_model)
     print(onnx.helper.printable_graph(onnx_model.graph))
