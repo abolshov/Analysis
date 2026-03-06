a0fe9454ea2e9cd81b471ff43bad9f7670888fa0
abolshov <artem.e.bolshov@gmail.com>
2026-03-06 10:28:25 -0600

    implement training file search

--- net/experimental/event_classification.py
+++ net/experimental/event_classification.py
@@ -10,8 +10,37 @@ if gpus:
         # Memory growth must be set before GPUs have been initialized
         print(e)
 
+import uproot
+import awkward as ak
+import numpy as np
+import os
+import re
+
 def main():
     print("Training DNN for event classification")
+
+    weight_file_pattern = re.compile(r"nParity(\d)_Merged_weight.root")
+    event_file_pattern = re.compile(r"nParity(\d)_Merged.root")
+
+    train_file_dir = "/home/artem/Desktop/CMS/data/DNN/SL/resolved/Run3_2022/Dataset"
+    event_files = {}
+    weight_files = {}
+    for f in os.listdir(train_file_dir):
+        weight_match = weight_file_pattern.search(f)
+        event_match = event_file_pattern.search(f)
+        abs_path = os.path.abspath(os.path.join(train_file_dir, f))
+        if weight_match:
+            parity = int(weight_match.group(1))
+            weight_files[parity] = abs_path
+        elif event_match:
+            parity = int(event_match.group(1))
+            event_files[parity] = abs_path
+        else:
+            continue
+
+    parity_file_map = { p: (event_files[p], weight_files[p]) for p in event_files.keys()}
+    for p, (event_file, weight_file) in parity_file_map.items():
+        print(f"Parity {p}: event_file={event_file}, weight_file={weight_file}")
 
 if __name__ == "__main__":
     main()
